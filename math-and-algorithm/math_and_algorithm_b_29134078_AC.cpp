20fcb347d7702f6e4bcdd67011b3bf51edf50589
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-06 15:28:53 +0000

    [AC] SubmissionID: 29134078 at 2022-02-06 15:28:53 (1644161333)

---
+++ math-and-algorithm/math_and_algorithm_b_29134078_AC.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29134078
+ * Submitted at: 2022-02-06 15:28:53
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+
+cin>>a;
+cin>>b;
+cin>>c;
+
+cout<<a+b+c<<endl;
+}

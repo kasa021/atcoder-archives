eeaa42c58c2392d81a6d03592945a92a09840c13
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-06 15:25:37 +0000

    [AC] SubmissionID: 29134050 at 2022-02-06 15:25:37 (1644161137)

---
+++ math-and-algorithm/math_and_algorithm_a_29134050_AC.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29134050
+ * Submitted at: 2022-02-06 15:25:37
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a,b;
+
+     a=5;
+     cin>>b;
+
+     cout<<a+b<<endl;
+}

efddeb5ad81695df022f533c86f8e3b946cd6559
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-06 15:40:52 +0000

    [WA] SubmissionID: 29134247 at 2022-02-06 15:40:52 (1644162052)

---
+++ math-and-algorithm/math_and_algorithm_c_29134247_WA.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29134247
+ * Submitted at: 2022-02-06 15:40:52
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_c
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int N;
+    int a;
+    int b=0;
+    
+    cin>>N;
+    
+    for(int i=0;i<N;i++){
+        cin>>a;
+        b+=a;
+    }
+
+    cout<<a<<endl;
+
+  }

4c69e9db460d07fbf7e12c21668c84059c70efe3
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-06 15:44:58 +0000

    [CE] SubmissionID: 29134313 at 2022-02-06 15:44:58 (1644162298)

---
+++ math-and-algorithm/math_and_algorithm_c_29134313_CE.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29134313
+ * Submitted at: 2022-02-06 15:44:58
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_c
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   int n;
+   int a[50];
+   int sum;
+
+   cin>>n;
+   sum=0;
+   for(int i=0;i<n;i+;){
+       cin>>a[i];
+       sum+=a[i];
+   }
+   cout<<sum<<endl;
+  }

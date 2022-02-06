5ea67b641d90ea72006668326e4d15d5874ea8ff
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-06 15:49:09 +0000

    [AC] SubmissionID: 29134364 at 2022-02-06 15:49:09 (1644162549)

---
+++ math-and-algorithm/math_and_algorithm_d_29134364_AC.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29134364
+ * Submitted at: 2022-02-06 15:49:09
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_d
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[3];
+    for (int i=0;i<3;i++){
+        cin>>a[i];
+    }
+    cout<<a[0]*a[1]*a[2]<<endl;
+
+}

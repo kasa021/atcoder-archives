66acbb9297605947e9bfb557fbf019d865f2ec32
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-07 09:20:51 +0000

    [WA] SubmissionID: 29144412 at 2022-02-07 09:20:51 (1644225651)

---
+++ abc085/abc085_a_29144412_WA.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc085/submissions/29144412
+ * Submitted at: 2022-02-07 09:20:51
+ * Problem URL: https://atcoder.jp/contests/abc085/tasks/abc085_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+
+    s[3]=8;
+
+    cout<<s<<endl;
+    
+}

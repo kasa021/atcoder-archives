8f87336f026350ac2a5ac5786e9fc474708bf888
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-07 03:31:08 +0000

    [WA] SubmissionID: 29139077 at 2022-02-07 03:31:08 (1644204668)

---
+++ math-and-algorithm/math_and_algorithm_a_29139077_WA.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29139077
+ * Submitted at: 2022-02-07 03:31:08
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_a
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<2*n+3<<endl;
+
+}

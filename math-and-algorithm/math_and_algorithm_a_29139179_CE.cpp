a92f3a70479b3b0adc207a9717e5bed02cf81290
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-07 03:38:59 +0000

    [CE] SubmissionID: 29139179 at 2022-02-07 03:38:59 (1644205139)

---
+++ math-and-algorithm/math_and_algorithm_a_29139179_CE.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29139179
+ * Submitted at: 2022-02-07 03:38:59
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_a
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    int x,y;
+    int a;
+
+    cin>>n;
+    cin>>x;
+    cin>>y;
+
+    for (int i=0;i<n;i++){
+        if(i%x==0)
+           a++
+    }
+
+    for(i=0;i<n;i+;){
+        if(i%y==0)
+          a++
+    }
+
+    cout<<a<,endl;
+
+}

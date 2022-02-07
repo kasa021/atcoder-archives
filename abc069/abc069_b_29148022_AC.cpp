8372a99a4943d2be5a6bd12ae9747ad59f2da617
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-07 12:47:04 +0000

    [AC] SubmissionID: 29148022 at 2022-02-07 12:47:04 (1644238024)

---
+++ abc069/abc069_b_29148022_AC.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc069/submissions/29148022
+ * Submitted at: 2022-02-07 12:47:04
+ * Problem URL: https://atcoder.jp/contests/abc069/tasks/abc069_b
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    string s;
+    cin>>s;
+    int n=s.size();
+    cout<<s[0]<<n-2<<s[n-1]<<endl;
+
+    return 0;
+}

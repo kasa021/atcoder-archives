173d4dc0c7e1892e0053f177ed08051b355389a5
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-07 09:35:26 +0000

    [CE] SubmissionID: 29144646 at 2022-02-07 09:35:26 (1644226526)

---
+++ abc069/abc069_b_29144646_CE.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc069/submissions/29144646
+ * Submitted at: 2022-02-07 09:35:26
+ * Problem URL: https://atcoder.jp/contests/abc069/tasks/abc069_b
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s[100];
+    string p[];
+    int i=0;
+
+    while (s[i]!='0'){
+      i++
+    }
+
+    i=i-2;
+    p[0]=s[0]; p[1]=i;p[2]=s[i+2];
+      
+    cout<<p<<endl;
+
+}

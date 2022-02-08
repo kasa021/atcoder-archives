e3d9d35a5901f5783ea7820fe56101ff00dc4220
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-08 15:04:31 +0000

    [AC] SubmissionID: 29169205 at 2022-02-08 15:04:31 (1644332671)

---
+++ math-and-algorithm/math_and_algorithm_r_29169205_AC.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29169205
+ * Submitted at: 2022-02-08 15:04:31
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_r
+ * Result: AC
+ * Execution Time: 38 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    long long A[200001];
+    long long  a=0,b=0,c=0,d=0;
+
+    cin>>n;
+    for(long long i=0;i<n;i++) cin>>A[i];
+   
+    for(long long i=0;i<n;i++){
+       if(A[i]==100)a++;
+       if(A[i]==200)b++;
+       if(A[i]==300)c++;
+       if(A[i]==400)d++;
+    }
+
+    cout<<a*d+b*c<<endl;
+    
+    return 0;
+    
+}

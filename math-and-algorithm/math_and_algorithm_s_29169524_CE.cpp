03c44c15eea703f955ac3df43a7c3d75bdea3dc4
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-08 15:19:57 +0000

    [CE] SubmissionID: 29169524 at 2022-02-08 15:19:57 (1644333597)

---
+++ math-and-algorithm/math_and_algorithm_s_29169524_CE.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/math-and-algorithm/submissions/29169524
+ * Submitted at: 2022-02-08 15:19:57
+ * Problem URL: https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_s
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    long long a[n];
+    long long b[3];
+
+    cin>>n;
+    for(long long i=0;i<n;i++) cin>>a[i];
+
+    for (long long i=0;i<n;i++){
+    switch(a[i]){
+        case 1: b[0]++; break;
+        case 2: b[1]++; break;
+        case 3: b[2]++; break;
+    }
+    }
+
+    cout<<(b[0]*b[0])/2+(b[1]*b[1])/2+(b[2]*b(2))/2<<endl;
+}

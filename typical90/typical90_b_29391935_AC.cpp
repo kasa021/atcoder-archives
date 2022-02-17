46e717d015cfd55ca5828d802bddce1b19472566
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-17 14:56:16 +0000

    [AC] SubmissionID: 29391935 at 2022-02-17 14:56:16 (1645109776)

---
+++ typical90/typical90_b_29391935_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/29391935
+ * Submitted at: 2022-02-17 14:56:16
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_b
+ * Result: AC
+ * Execution Time: 178 ms
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool hantei(string S) {
+    int x=0;
+
+    for(int i = 0; i < S.size(); i++){
+        if(S[i] == '(') x+=1;
+        if(S[i] == ')') x-=1;
+        if(x<0) return false;
+    }
+    if(x==0) return true;
+    return false;
+
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0;i<(1<<n); i++){
+        string a = "";
+        for(int j= n-1; j>=0; j--){
+            if((i &(1<<j)) == 0){ a +="(";}
+            else {a+=")";}
+        }
+        bool b = hantei(a);
+        if(b == true ) cout<<a<<endl;
+    }
+ return 0;
+
+}

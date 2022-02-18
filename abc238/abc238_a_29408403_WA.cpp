20a6b4f33356f33872d63aba5fc2afcd22e58406
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-18 14:57:58 +0000

    [WA] SubmissionID: 29408403 at 2022-02-18 14:57:58 (1645196278)

---
+++ abc238/abc238_a_29408403_WA.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc238/submissions/29408403
+ * Submitted at: 2022-02-18 14:57:58
+ * Problem URL: https://atcoder.jp/contests/abc238/tasks/abc238_a
+ * Result: WA
+ * Execution Time: 14 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    if(n>=5){printf("Yes\n");}
+    if(n<=4){printf("No\n");}
+
+    return 0;
+}

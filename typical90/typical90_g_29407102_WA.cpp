704d80b8c419dc031bafda4c1910a5171b5910eb
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-18 13:41:49 +0000

    [WA] SubmissionID: 29407102 at 2022-02-18 13:41:49 (1645191709)

---
+++ typical90/typical90_g_29407102_WA.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/29407102
+ * Submitted at: 2022-02-18 13:41:49
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_g
+ * Result: WA
+ * Execution Time: 654 ms
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+const int INF = 2000000000;
+int N;
+int A[300009];
+int Q;
+int B[300009];
+    
+int main(){
+    cin>>N;
+    for(int  i=1;i<=N;i++)  cin>>A[i];
+    cin>>Q;
+    for(int i=0;i<=Q;i++) cin>>B[i];
+
+    sort(A + 1, A + N + 1);//sortする
+
+    for(int i=1;i<=Q;i++){
+        int pos1=lower_bound(A+1,A+N+1,B[i]) - A; //これでb[i]以上の左側のa[k]がわかるらしい。
+        int px=INF, py=INF;
+
+        if(pos1<=N) px=abs(B[i] - A[pos1]);
+        if(pos1>=2) py=abs(B[i] -A[pos1 - 1]);
+        cout<<min(px,py)<<endl;
+    }
+    return 0;
+    
+}

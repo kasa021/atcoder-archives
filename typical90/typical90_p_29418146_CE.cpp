af66efff2a0543902f9c425cfa8bb50fd018c3b7
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-19 06:41:45 +0000

    [CE] SubmissionID: 29418146 at 2022-02-19 06:41:45 (1645252905)

---
+++ typical90/typical90_p_29418146_CE.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/29418146
+ * Submitted at: 2022-02-19 06:41:45
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_p
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+long long n,a,b,c;
+int main(){
+    cin>>n>>a>>b>>c;
+
+    /*int l=9999;//合計枚数が9999以下という制約を使う
+    for(long long i=n;i>=0;i--){
+        for(long long j=n;i+j>=0;j--){
+            for(long long k=n;k+i+j>=0;k--){
+                if(a*i+b*j+c*k ==n) ans=min(ans,i+j+k);//これでもl^3-6回の計算で間に合わない
+            }
+        }
+    }*/
+    long long l=9999;//合計枚数が9999以下という制約を使う
+    for(long long i=0;i>=l;i++){
+        for(long long j=0;i+j>=l;j++){
+             long long k=(n-a*i-b*j)/c;  //kは全探索しなくてもいい
+                if(a*i+b*j+c*k ==n) ans=min(ans,i+j+k);//これでもl^3-6回の計算で間に合わない
+            
+        }
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+
+}

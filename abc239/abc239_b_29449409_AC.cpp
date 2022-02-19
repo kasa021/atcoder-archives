b9f316988aa50f19bbe0b5ded95e7d6297af7c88
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-19 12:27:26 +0000

    [AC] SubmissionID: 29449409 at 2022-02-19 12:27:26 (1645273646)

---
+++ abc239/abc239_b_29449409_AC.cpp
@@ -0,0 +1,24 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc239/submissions/29449409
+ * Submitted at: 2022-02-19 12:27:26
+ * Problem URL: https://atcoder.jp/contests/abc239/tasks/abc239_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+double long x,y,z;
+int main(){
+   cin>>x;
+   y=x/10;
+
+   z=floor(y);
+
+   printf("%-17.0Lf\n",z);
+
+   return 0;
+}

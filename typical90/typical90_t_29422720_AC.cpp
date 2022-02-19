2ec7e157020f37469251342342fa651c8389e1dc
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-19 10:01:58 +0000

    [AC] SubmissionID: 29422720 at 2022-02-19 10:01:58 (1645264918)

---
+++ typical90/typical90_t_29422720_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/29422720
+ * Submitted at: 2022-02-19 10:01:58
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_t
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+double long a;
+double long b,c;
+
+
+int main(){
+  cin>>a>>b>>c;
+
+  
+  
+  if(a<pow(c,b)) cout<<"Yes"<<endl;
+  else cout<<"No"<<endl;
+
+ 
+
+ return 0;
+}

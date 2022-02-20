60858843c015a5bbf815789ce42acb6de38a5cdb
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-02-20 07:12:35 +0000

    [TLE] SubmissionID: 29495117 at 2022-02-20 07:12:35 (1645341155)

---
+++ typical90/typical90_af_29495117_TLE.cpp
@@ -0,0 +1,54 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/29495117
+ * Submitted at: 2022-02-20 07:12:35
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_af
+ * Result: TLE
+ * Execution Time: 5513 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[12][12];
+int m;
+int x[1<<18];
+int y[1<<18];
+
+bool kenaku[12][12];
+int ans=(1<<30);
+
+int main(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;i<=n;j++){
+            cin>>a[i][j]; //選手iが区間jを走る時間
+        }
+    }
+    cin>>m;
+    for(int i=1;i<=m;i++) cin>>x[i]>>y[i]; //仲悪い奴
+    
+    vector<int> vec;
+    for(int i=1;i<=n;i++) vec.push_back(i); //push_back()は末尾に要素を付け足す,今回は要素をn個にするということ
+    for(int i=1;i<=m;i++){
+        kenaku[x[i]][y[i]] = true;
+        kenaku[y[i]][x[i]] =true;
+    }
+
+    do{
+        bool flag=true;
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+            if(kenaku[vec[i]][vec[i+1]] ==true) flag=false;
+        }
+        for(int i=0;i<n;i++) sum+=a[vec[i]][i+1];
+        if(flag==true) ans=min(ans,sum);
+    }while(next_permutation(vec.begin(),vec.end()));
+
+    if(ans==(1<<30)) ans=-1;
+    cout<<ans<<endl;
+    return 0;
+
+   
+}

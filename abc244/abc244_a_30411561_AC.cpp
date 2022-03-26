e8c4b6232eac524f2c9785b9b0870a3d32a04cc2
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-03-26 04:29:34 +0000

    [AC] SubmissionID: 30411561 at 2022-03-26 04:29:34 (1648268974)

---
+++ abc244/abc244_a_30411561_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc244/submissions/30411561
+ * Submitted at: 2022-03-26 04:29:34
+ * Problem URL: https://atcoder.jp/contests/abc244/tasks/abc244_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s>>s;
+    cout<<s.back()<<endl;
+
+}

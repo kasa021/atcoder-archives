5f5bea4a1d563d1e563358ce9d6a8f47936faa45
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-03-27 08:22:28 +0000

    [AC] SubmissionID: 30506187 at 2022-03-27 08:22:28 (1648369348)

---
+++ abc245/abc245_c_30506187_AC.cpp
@@ -0,0 +1,42 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc245/submissions/30506187
+ * Submitted at: 2022-03-27 08:22:28
+ * Problem URL: https://atcoder.jp/contests/abc245/tasks/abc245_c
+ * Result: AC
+ * Execution Time: 122 ms
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+signed main(){
+    ll n,k;cin>>n>>k;
+    vector<ll>a(n+1),b(n+1);
+    for(ll i=1;i<=n;i++)cin>>a[i];
+    for(ll i=1;i<=n;i++)cin>>b[i];
+    vector<ll>dp(n+1,false),ep(n+1,false);
+
+    //初期値
+    dp[1]=ep[1]=true;
+
+    //遷移
+    for(ll i=2;i<=n;i++){
+        if(dp[i-1]){
+            if(abs(a[i-1]-a[i])<=k)dp[i]=true;
+            if(abs(a[i-1]-b[i])<=k)ep[i]=true;
+        }
+        if(ep[i-1]){
+            if(abs(b[i-1]-a[i])<=k)dp[i]=true;
+            if(abs(b[i-1]-b[i])<=k)ep[i]=true;
+        }
+    }
+
+    //答え
+    if(dp[n]||ep[n])cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+
+    return 0;
+}

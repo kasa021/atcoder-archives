63beb76ee8bc9c02f27e02acf9f19b48b807786e
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-03-29 04:06:50 +0000

    [AC] SubmissionID: 30542638 at 2022-03-29 04:06:50 (1648526810)

---
+++ abc245/abc245_c_30542638_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc245/submissions/30542638
+ * Submitted at: 2022-03-29 04:06:50
+ * Problem URL: https://atcoder.jp/contests/abc245/tasks/abc245_c
+ * Result: AC
+ * Execution Time: 131 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(n);i++)
+
+int main(){
+    int n,k;
+    cin>> n >> k;
+    vector<vector<int>> a(2,vector<int>(n));
+
+    rep(i,2)rep(j,n) cin>>a[i][j];
+    vector<vector<bool>> dp(n,vector<bool>(2));
+
+    dp[0][0]=dp[0][1]=true;
+
+    for(int i=1;i<n;i++){
+        rep(j,2){
+            int pi=i-1;
+            rep(pj,2){
+                if(!dp[pi][pj]) continue;
+                if(abs(a[pj][pi]-a[j][i])>k) continue;
+                dp[i][j]=true;
+            }
+        }
+    }
+
+    rep(j,2){
+        if(dp[n-1][j]){
+            cout<<"Yes"<< endl;
+            return 0;
+        }
+    }
+    cout<<"No"<<endl;
+    return 0;
+}

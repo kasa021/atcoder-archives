f1ac6e8bc047c66cdaf084aba733aad393e45d1d
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-02 13:10:19 +0000

    [WA] SubmissionID: 30660725 at 2022-04-02 13:10:19 (1648905019)

---
+++ abc246/abc246_c_30660725_WA.cpp
@@ -0,0 +1,68 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc246/submissions/30660725
+ * Submitted at: 2022-04-02 13:10:19
+ * Problem URL: https://atcoder.jp/contests/abc246/tasks/abc246_c
+ * Result: WA
+ * Execution Time: 2206 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+
+int N, A[200009], C[200009];
+
+
+void MergeSort(int l, int r) {
+	
+	if (r - l == 1) return;
+	
+	
+	int m = (l + r) / 2;
+	MergeSort(l, m);
+	MergeSort(m, r);
+	
+	int c1 = l, c2 = m, cnt = 0;
+	while (c1 != m || c2 != r) {
+		if (c1 == m) {
+			
+			C[cnt] = A[c2]; c2++;
+		}
+		else if (c2 == r) {
+			
+			C[cnt] = A[c1]; c1++;
+		}
+		else {
+			
+			if (A[c1] <= A[c2]) {
+				C[cnt] = A[c1]; c1++;
+			}
+			else {
+				C[cnt] = A[c2]; c2++;
+			}
+		}
+		cnt++;
+	}
+	for (int i = 0; i < cnt; i++) A[l + i] = C[i];
+}
+
+
+int main(){
+    int n,k,x;
+    vector<int> a(200009);
+
+    cin>>n>>k>>x;
+    rep(i,n) cin>>a[i];
+
+    for(int i=k;i>0;i--){
+        MergeSort(1, n+1);
+        a[n] = max(a[n]-x,0); 
+    }
+    int ans=0;
+    rep(i,n) ans+=a[i];
+
+    cout<<ans<<endl;
+    return 0;
+
+}

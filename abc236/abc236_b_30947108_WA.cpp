2e09fbb00c1d6071d2ecfc3400c5b90c8469d3db
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-13 10:59:21 +0000

    [WA] SubmissionID: 30947108 at 2022-04-13 10:59:21 (1649847561)

---
+++ abc236/abc236_b_30947108_WA.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc236/submissions/30947108
+ * Submitted at: 2022-04-13 10:59:21
+ * Problem URL: https://atcoder.jp/contests/abc236/tasks/abc236_b
+ * Result: WA
+ * Execution Time: 82 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> count(n+1);
+    for(int i=0;i<4*n-1;i++){
+        int a;
+        cin>>a;
+        count[a] +=1;
+    }
+    for(int i=0;i<4;i++){
+        if(count[i] == 3)
+        cout<<i<<endl;
+    }
+    return 0;
+}

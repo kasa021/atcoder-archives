ef59a4bb3ddfc166d464a3e5c125cd9b3b3fb690
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-16 12:28:07 +0000

    [WA] SubmissionID: 31023139 at 2022-04-16 12:28:07 (1650112087)

---
+++ abc248/abc248_b_31023139_WA.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc248/submissions/31023139
+ * Submitted at: 2022-04-16 12:28:07
+ * Problem URL: https://atcoder.jp/contests/abc248/tasks/abc248_b
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+
+
+int main(){
+    int a,b,k;
+    cin>>a>>b>>k;
+    int ans=a;
+    int i=0;
+
+    while((double)ans/2<(double)b/2){
+        ans *=k;
+        ++i;
+    }
+    cout<<i<<endl;
+    return 0;
+}

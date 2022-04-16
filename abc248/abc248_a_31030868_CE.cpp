6dde93db727f355184f32b7d0ebdd17df1ab805d
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-16 12:52:53 +0000

    [CE] SubmissionID: 31030868 at 2022-04-16 12:52:53 (1650113573)

---
+++ abc248/abc248_a_31030868_CE.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc248/submissions/31030868
+ * Submitted at: 2022-04-16 12:52:53
+ * Problem URL: https://atcoder.jp/contests/abc248/tasks/abc248_a
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+
+
+int main(){
+    string s;
+    cin>>s;
+    vector<int> a[10];
+    rep(i,10) cin>>a[i] =0;
+    rep(i,10){
+        rep(j,10){
+            if(s[i] != 'j') a[j] = 1; 
+        }
+    }
+
+    rep(i,10){
+        if(a[i] == 1) cout<<s[i]<<endl;
+    }
+    return 0;
+
+
+}

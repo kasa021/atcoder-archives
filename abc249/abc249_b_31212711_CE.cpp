9ee35db97daf83eef1337ae3e1a4548b2779a0c8
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-23 14:08:56 +0000

    [CE] SubmissionID: 31212711 at 2022-04-23 14:08:56 (1650722936)

---
+++ abc249/abc249_b_31212711_CE.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc249/submissions/31212711
+ * Submitted at: 2022-04-23 14:08:56
+ * Problem URL: https://atcoder.jp/contests/abc249/tasks/abc249_b
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i,n) for(int i=1; i<=(n); ++i)
+using ll = long long;
+
+
+int main(){
+    string s;
+    cin>>s;
+
+    bool upper = false; lower =false;
+    set<char> st;
+    for(char c:s){
+        if(isupper(c)) upper = true;
+        if(islower(c)) lower = true;
+        st.insert(c);
+    }
+    if(upper&&lower&&st.size() == s.size()){//st.size() == s.size()が文字が重複してない条件
+        cout<<"Yes"<<endl;
+    }
+    else cout<<"No"<<endl;
+    return 0;
+
+}

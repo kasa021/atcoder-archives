62f8ec47270c8f4024c182f4d5ed60a0523906c9
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-23 13:39:50 +0000

    [WA] SubmissionID: 31210091 at 2022-04-23 13:39:50 (1650721190)

---
+++ abc249/abc249_c_31210091_WA.cpp
@@ -0,0 +1,45 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc249/submissions/31210091
+ * Submitted at: 2022-04-23 13:39:50
+ * Problem URL: https://atcoder.jp/contests/abc249/tasks/abc249_c
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i,n) for(int i=1; i<=(n); ++i)
+using ll = long long;
+
+
+int main(){
+    string l;
+    l= "abcdefghijklmnopqrstuwxyz";
+    int n,k;
+    cin>>n>>k;
+    multiset<char> ms;
+    for(int i=1;i<=n;i++){
+        string s;
+        cin>>s;
+        for(int j=0;j<(int)s.size();j++){
+            char S =s[j];
+            ms.insert(S);
+        }
+    }
+    int a[30];
+    for(int i=0;i<26;i++){
+       
+            char S =l[i];
+        a[i] = ms.count(S);
+        
+    }
+    int ans=0;
+    for(int i=0;i<26;i++){
+        if(a[i]>=k) ans++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}

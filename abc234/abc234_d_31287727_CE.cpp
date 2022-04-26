fb66650a6f1522bf24dea5108f608d05dfdcdc1f
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-26 12:52:41 +0000

    [CE] SubmissionID: 31287727 at 2022-04-26 12:52:41 (1650977561)

---
+++ abc234/abc234_d_31287727_CE.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc234/submissions/31287727
+ * Submitted at: 2022-04-26 12:52:41
+ * Problem URL: https://atcoder.jp/contests/abc234/tasks/abc234_d
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+using ll = long long;
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    vector<int> p(n);
+    rep(i,n) cin>> p[i];
+    priority_queue<int,vector<int>,greater<int>> q;
+    rep(i,k-1) q.push(p[i]);
+    for(int i=k-1;i<n;++i){
+        q.push(p[i])+
+        while(q.size()>k) q.pop();
+        cout<<q.top()<<endl;
+    }
+    return 0;
+}

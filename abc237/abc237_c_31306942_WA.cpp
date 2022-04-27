2047afff6b5687c9859824a2343c42675c159f89
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-04-27 13:20:37 +0000

    [WA] SubmissionID: 31306942 at 2022-04-27 13:20:37 (1651065637)

---
+++ abc237/abc237_c_31306942_WA.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc237/submissions/31306942
+ * Submitted at: 2022-04-27 13:20:37
+ * Problem URL: https://atcoder.jp/contests/abc237/tasks/abc237_c
+ * Result: WA
+ * Execution Time: 30 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+
+int main(){
+    string s;
+    cin>>s;
+    bool ans = true;
+    int i=0,j=s.size() - 1;
+    while(i<j){
+        if(s[j] == s[i]) i++,j--;
+        else if(s[i] == 'a') i++;
+        else if(s[j] == 'a') j--;
+        else {ans=false; break;}
+    }
+    if(ans ==true) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+
+    return 0;
+}

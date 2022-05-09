2ed4ac9aaed73c64940fc7237556fae490eedd4d
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-09 11:41:47 +0000

    [WA] SubmissionID: 31569715 at 2022-05-09 11:41:47 (1652096507)

---
+++ abc250/abc250_c_31569715_WA.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc250/submissions/31569715
+ * Submitted at: 2022-05-09 11:41:47
+ * Problem URL: https://atcoder.jp/contests/abc250/tasks/abc250_c
+ * Result: WA
+ * Execution Time: 69 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,q;
+    cin>>n>>q;
+    vector<int> x(q);
+    vector<int> v(n+1),p(n+1);
+
+    for(int i=1;i<=n;i++) v[i] = i,p[i] = i;
+    for(int i=0;i<q;i++) cin>>x[i];
+
+    for(int i=0;i<=q;i++){
+        int p0=p[x[i]];
+        int p1=p0;
+        if(p0==n) p1--;
+        else p1++;
+        int v0=v[p0];
+        int v1=v[p1];
+        swap(v[p0],v[p1]);
+        swap(p[v0],p[p1]);
+    }
+    for(int i=1;i<=n;i++){
+        if(i!=1)cout<<' ';
+        cout<<v[i];
+    }
+    cout<<endl;
+
+    return 0;
+
+    
+}

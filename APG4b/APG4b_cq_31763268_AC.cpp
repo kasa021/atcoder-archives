0ccf78414081b59455ab84bdbc83d8e0b1fb96c3
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-17 09:50:53 +0000

    [AC] SubmissionID: 31763268 at 2022-05-17 09:50:53 (1652781053)

---
+++ APG4b/APG4b_cq_31763268_AC.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/31763268
+ * Submitted at: 2022-05-17 09:50:53
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cq
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  string op;
+  cin >> A >> op >> B;
+
+  if (op == "+") {
+    cout << A + B << endl;
+  }else if (op == "-") {
+    cout << A - B << endl;
+  }else if (op == "*") {
+    cout << A * B << endl;
+  }else if (op == "/") {
+      if(B==0) cout<<"error"<<endl;
+      else cout << A / B << endl;
+  }else if (op == "?"||op=="="||op=="!") {
+    cout << "error"<< endl;
+  }
+  return 0;
+}

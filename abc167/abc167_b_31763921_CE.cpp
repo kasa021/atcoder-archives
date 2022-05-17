f235bcc1af2c6177b424b709dfc9883c5336bf93
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-17 10:34:58 +0000

    [CE] SubmissionID: 31763921 at 2022-05-17 10:34:58 (1652783698)

---
+++ abc167/abc167_b_31763921_CE.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc167/submissions/31763921
+ * Submitted at: 2022-05-17 10:34:58
+ * Problem URL: https://atcoder.jp/contests/abc167/tasks/abc167_b
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int ans = 0;
+    int a,b,c,k;
+    cin>>a>>b>>c>>k;
+    
+      while(a>0&&k>0) {
+          ans +=a;
+          a--;
+          k--;
+    }
+     while(b>0&&k>0) {
+          k--;
+    }
+     while(c>0&&k>0) {
+          ans +=c;
+          c--;
+          k--;
+    }
+
+    cout<<ans<<ensl;
+
+}

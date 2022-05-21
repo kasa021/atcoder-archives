a6eab53eba3dedf1ccc79daf41e6603c293cf972
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-21 13:20:05 +0000

    [CE] SubmissionID: 31870116 at 2022-05-21 13:20:05 (1653139205)

---
+++ abc252/abc252_c_31870116_CE.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc252/submissions/31870116
+ * Submitted at: 2022-05-21 13:20:05
+ * Problem URL: https://atcoder.jp/contests/abc252/tasks/abc252_c
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i,n) for(int i=1; i<(n); ++i)
+using ll = long long;
+
+
+int main(){
+    int n;
+    cin>>n;
+    set<int> p[10];
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        int count;
+        for(int j=0;j<10;j++){
+            for(int k=0;k<10;k++){
+                if(s[k] == '0' + j){
+                    count = p[j].size();
+                    p[j].insert(k);
+                    if(count==(int)p[j].size()){
+                        int b=*rbegin(p[j])+10;
+                        p[j].insert(b);
+                    }
+
+                }
+            }
+        }
+
+    }
+    vector<int> a(10);
+    for(int i=0;i<10;i++){
+        a[i] = *rbegin(p[i]);
+}
+
+    int ans= 10000000;
+    rep(i,10) ans=min(ans,a[i]);
+    cout<<ans<<end;
+    
+}

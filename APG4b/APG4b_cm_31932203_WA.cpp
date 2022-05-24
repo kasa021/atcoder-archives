6db596cdc6c49b45a234d0bed1e0b16eff4b6d72
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-24 09:29:38 +0000

    [WA] SubmissionID: 31932203 at 2022-05-24 09:29:38 (1653384578)

---
+++ APG4b/APG4b_cm_31932203_WA.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/31932203
+ * Submitted at: 2022-05-24 09:29:38
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cm
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<"A:";
+    rep(i,a)cout<<"]";
+    cout<<" "<<endl;
+    cout<<"B:";
+    rep(i,b)cout<<"]";
+    cout<<endl;
+
+    return 0;
+}

c0090033889d2157e9976447767825fde77a0163
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-28 08:31:47 +0000

    [AC] SubmissionID: 31993040 at 2022-05-28 08:31:47 (1653726707)

---
+++ abc240/abc240_c_31993040_AC.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc240/submissions/31993040
+ * Submitted at: 2022-05-28 08:31:47
+ * Problem URL: https://atcoder.jp/contests/abc240/tasks/abc240_c
+ * Result: AC
+ * Execution Time: 243 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+using ll = long long;
+
+int main(){
+    int n,x;
+    cin>>n>>x;
+    vector<int> a(n), b(n);
+    rep(i,n) cin>>a[i]>>b[i];
+    vector dp(10000,vector<int>(x+1,0));
+    dp[0][0]=1;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<=x;++j){
+            if(dp[i][j]){
+                if(j+a[i]<=x) dp[i+1][j+a[i]]=1;
+            
+            if(j+b[i]<=x) dp[i+1][j+b[i]]=1;
+            }
+        }
+    }
+    cout << (dp[n][x] ? "Yes" : "No") << '\n';
+    
+}

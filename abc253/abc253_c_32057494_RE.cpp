8fc5d30179836fa3dc2fe114cf7519c19a5ce017
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-28 13:47:51 +0000

    [RE] SubmissionID: 32057494 at 2022-05-28 13:47:51 (1653745671)

---
+++ abc253/abc253_c_32057494_RE.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc253/submissions/32057494
+ * Submitted at: 2022-05-28 13:47:51
+ * Problem URL: https://atcoder.jp/contests/abc253/tasks/abc253_c
+ * Result: RE
+ * Execution Time: 105 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+using ll = long long;
+using P = pair<int,int>;
+
+int main(){
+    int q;
+    cin>>q;
+    int b,c;
+    set<int> st;
+    vector<int> cnt(1000000000,0);
+
+    for(int i=1;i<=q;i++){
+        int a;
+        cin>>a;
+        if(a==1){
+            int x;
+            cin>>x;
+            cnt[x]++;
+            st.insert(x);
+        }
+        if(a==2){
+            int x;
+            cin>>x>>c;
+            b=min(c,cnt[x]);
+            cnt[x]-=b;
+            if(cnt[x]==0) st.erase(x);
+        }
+        if(a==3){
+           int k=*end(st);
+           int l=*begin(st);
+           cout<<k-l<<endl;
+        }
+    }
+    
+    return 0;
+}

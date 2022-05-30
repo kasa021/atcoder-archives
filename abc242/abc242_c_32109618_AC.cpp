02b79e7bf10180d835f0f90ed3c5b3d168e93c5c
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-05-30 09:49:19 +0000

    [AC] SubmissionID: 32109618 at 2022-05-30 09:49:19 (1653904159)

---
+++ abc242/abc242_c_32109618_AC.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc242/submissions/32109618
+ * Submitted at: 2022-05-30 09:49:19
+ * Problem URL: https://atcoder.jp/contests/abc242/tasks/abc242_c
+ * Result: AC
+ * Execution Time: 77 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+using ll = long long;
+const int mod = 998244353;
+void add(int& x,int y){
+    x+=y;
+    x%=mod;
+}
+
+int dp[1000005][10];
+
+int main(){
+    int n;
+    cin>>n;
+    for(int d=1;d<=9;d++) dp[1][d]=1;
+    for(int i=2;i<=n;i++){
+        for(int d=1;d<=9;d++){
+            if(d-1>=1) add(dp[i][d],dp[i-1][d-1]);
+            add(dp[i][d],dp[i-1][d]);
+            if(d+1<=9) add(dp[i][d],dp[i-1][d+1]);
+        }
+    }
+    int ans=0;
+    for(int d=1;d<=9;d++) add(ans,dp[n][d]);
+    cout<<ans<<endl;
+    return 0;
+
+}

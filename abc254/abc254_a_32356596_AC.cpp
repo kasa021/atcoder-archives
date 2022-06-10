d8555ef1f1adb8af6d79255296174d19f3ccbee9
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-06-10 13:27:21 +0000

    [AC] SubmissionID: 32356596 at 2022-06-10 13:27:21 (1654867641)

---
+++ abc254/abc254_a_32356596_AC.cpp
@@ -0,0 +1,24 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc254/submissions/32356596
+ * Submitted at: 2022-06-10 13:27:21
+ * Problem URL: https://atcoder.jp/contests/abc254/tasks/abc254_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+using ll = long long;
+using P = pair<int,int>;
+
+int main(){
+    string s;
+    cin>>s;
+    cout<<s[1]<<s[2]<<endl;
+    return 0;
+
+}

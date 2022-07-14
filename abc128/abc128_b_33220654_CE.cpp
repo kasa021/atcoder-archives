07eeb43a423f53e4164a9b463f82eac1e42f732f
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-07-14 11:18:32 +0000

    [CE] SubmissionID: 33220654 at 2022-07-14 11:18:32 (1657797512)

---
+++ abc128/abc128_b_33220654_CE.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc128/submissions/33220654
+ * Submitted at: 2022-07-14 11:18:32
+ * Problem URL: https://atcoder.jp/contests/abc128/tasks/abc128_b
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+typedef pair<ll, ll> P;
+typedef tuple<string, ll, ll> T;
+using ll = long long ;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<T> r;
+    rep(i,n){
+        int p;string s;
+        cin>>s>>p;
+        r..push_back(T(s,100-p,i));
+    }
+    sort(r.begin(),rr.end());
+    for(auto [S,P,idx]:r){
+        cout<<idx+1<<endl;
+    }
+    return 0;
+}

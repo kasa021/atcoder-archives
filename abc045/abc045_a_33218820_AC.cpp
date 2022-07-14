8cedffbdbb37bd78239221eb5260d94cc2a061e6
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-07-14 09:17:20 +0000

    [AC] SubmissionID: 33218820 at 2022-07-14 09:17:20 (1657790240)

---
+++ abc045/abc045_a_33218820_AC.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc045/submissions/33218820
+ * Submitted at: 2022-07-14 09:17:20
+ * Problem URL: https://atcoder.jp/contests/abc045/tasks/abc045_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define graph=<vector<vector<int>>
+using ll = long long ;
+
+int main() {
+    int a,b,h;
+    cin>>a>>b>>h;
+    int ans=(a+b)*h/2;
+cout<<ans<<endl;
+    return 0;
+}

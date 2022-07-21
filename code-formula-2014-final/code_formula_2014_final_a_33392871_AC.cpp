b1155a181e6690a000e565ce671d6076775b15f8
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-07-21 09:27:51 +0000

    [AC] SubmissionID: 33392871 at 2022-07-21 09:27:51 (1658395671)

---
+++ code-formula-2014-final/code_formula_2014_final_a_33392871_AC.cpp
@@ -0,0 +1,24 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/code-formula-2014-final/submissions/33392871
+ * Submitted at: 2022-07-21 09:27:51
+ * Problem URL: https://atcoder.jp/contests/code-formula-2014-final/tasks/code_formula_2014_final_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+using ll = long long ;
+typedef pair<ll, ll> P;
+typedef tuple<string, ll, ll> T;
+
+int main() {
+    int a,b;
+  cin>>a>>b;
+    cout<<a*b<<endl;
+
+    return 0;
+}

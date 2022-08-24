5883f56605588049bd21c7f6980079c4ba4e7990
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-08-24 12:16:13 +0000

    [TLE] SubmissionID: 34312102 at 2022-08-24 12:16:13 (1661343373)

---
+++ abc265/abc265_c_34312102_TLE.cpp
@@ -0,0 +1,71 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc265/submissions/34312102
+ * Submitted at: 2022-08-24 12:16:13
+ * Problem URL: https://atcoder.jp/contests/abc265/tasks/abc265_c
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define graph vector<vector<int>>
+#define all(vec) vec.begin(), vec.end()
+using ll = long long ;
+typedef pair<int, int> P;
+typedef tuple<string, ll, ll> T;
+
+int main() {
+   int h,w;
+   cin>>h>>w;
+   string s[h];
+   rep(i,h){
+    cin>>s[i];
+   }
+   int i=0,j=0;
+
+   while(1){
+    if(s[i][j]=='U'&&i!=0){
+        s[i][j]='a';
+        i--;
+        continue;
+    }
+    if(s[i][j]=='D'&&i!=h-1){
+        s[i][j]=='a';
+        i++;
+        continue;
+    }
+    if(s[i][j]=='L'&&j!=0){
+        s[i][j]=='a';
+        j--;
+        continue;
+    }
+    if(s[i][j]=='R'&&j!=w-1){
+        s[i][j]=='a';
+        j++;
+        continue;
+    }
+    if(s[i][j]=='a'){
+        cout<<"-1"<<endl;
+        return 0;
+    }
+    if(s[i][j]=='U'&&i==0){
+        cout<<i+1<<" "<<j+1<<endl;
+        return 0;
+    }
+    if(s[i][j]=='D'&&i==h-1){
+        cout<<i+1<<" "<<j+1<<endl;
+        return 0;
+    }
+     if(s[i][j]=='L'&&j==0){
+        cout<<i+1<<" "<<j+1<<endl;
+        return 0;
+    }
+     if(s[i][j]=='R'&&j==w-1){
+        cout<<i+1<<" "<<j+1<<endl;
+        return 0;
+    }
+   }
+}

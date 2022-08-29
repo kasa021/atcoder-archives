9c44958eba8e0db33baecc61a446fbbae4ff40ca
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-08-29 13:16:39 +0000

    [AC] SubmissionID: 34445176 at 2022-08-29 13:16:39 (1661778999)

---
+++ abc255/abc255_b_34445176_AC.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc255/submissions/34445176
+ * Submitted at: 2022-08-29 13:16:39
+ * Problem URL: https://atcoder.jp/contests/abc255/tasks/abc255_b
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int n,k;
+  cin >> n >> k;
+  vector<int> a(k);
+  for(auto &nx : a){
+    cin >> nx;
+    nx--;
+  }
+  vector<long long> x(n),y(n);
+  for(int i=0;i<n;i++){cin >> x[i] >> y[i];}
+
+  long long res=0;
+  for(int i=0;i<n;i++){
+    long long cres=8e18;
+    for(auto &nx : a){
+      cres=min(cres,(x[i]-x[nx])*(x[i]-x[nx]) + (y[i]-y[nx])*(y[i]-y[nx]));
+    }
+    res=max(res,cres);
+  }
+  printf("%.12lf\n",sqrt((double)res));
+  return 0;
+}
+

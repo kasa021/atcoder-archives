63ce65bb457873428a2c8226802dab5a6812ddd3
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-09-05 09:26:19 +0000

    [AC] SubmissionID: 34634523 at 2022-09-05 09:26:19 (1662369979)

---
+++ abc231/abc231_a_34634523_AC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc231/submissions/34634523
+ * Submitted at: 2022-09-05 09:26:19
+ * Problem URL: https://atcoder.jp/contests/abc231/tasks/abc231_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      double a;
+      cin>>a;
+      a/=100;
+      printf("%.10f\n", a);
+    }

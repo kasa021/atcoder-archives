f57de4d810063c092d532e843fdcc36034ea39e5
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-09-06 02:39:52 +0000

    [AC] SubmissionID: 34646798 at 2022-09-06 02:39:52 (1662431992)

---
+++ abc242/abc242_b_34646798_AC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc242/submissions/34646798
+ * Submitted at: 2022-09-06 02:39:52
+ * Problem URL: https://atcoder.jp/contests/abc242/tasks/abc242_b
+ * Result: AC
+ * Execution Time: 18 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      string s;
+      cin>>s;
+      sort(s.begin(),s.end());
+      cout<< s << endl;
+    }

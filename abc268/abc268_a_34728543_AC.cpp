7c9cb8264a78f57a507c306304312a9889a2037a
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-09-10 12:02:43 +0000

    [AC] SubmissionID: 34728543 at 2022-09-10 12:02:43 (1662811363)

---
+++ abc268/abc268_a_34728543_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc268/submissions/34728543
+ * Submitted at: 2022-09-10 12:02:43
+ * Problem URL: https://atcoder.jp/contests/abc268/tasks/abc268_a
+ * Result: AC
+ * Execution Time: 16 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      int a,b,c,d,e;
+      cin>>a>>b>>c>>d>>e;
+      set<int> s;
+      s.insert(a);
+      s.insert(b);
+      s.insert(c);
+      s.insert(d);
+      s.insert(e);
+      int size=s.size();
+      cout<<size<<endl;
+
+
+
+
+     
+    }

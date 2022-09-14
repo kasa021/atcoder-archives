e92f357a4ed1079c03da0d4b061e1f8a1c22c988
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-09-14 09:16:25 +0000

    [WA] SubmissionID: 34852501 at 2022-09-14 09:16:25 (1663146985)

---
+++ abc014/abc014_1_34852501_WA.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc014/submissions/34852501
+ * Submitted at: 2022-09-14 09:16:25
+ * Problem URL: https://atcoder.jp/contests/abc014/tasks/abc014_1
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      int a,b;
+      cin>>a>>b;
+      int ans=a%b;
+      cout<<b-ans<<endl;
+    }

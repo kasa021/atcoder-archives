d737b40ac65e2b9f47fd187b664c262e8b728085
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-09-20 05:27:21 +0000

    [WA] SubmissionID: 35008152 at 2022-09-20 05:27:21 (1663651641)

---
+++ tessoku-book/tessoku_book_h_35008152_WA.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/tessoku-book/submissions/35008152
+ * Submitted at: 2022-09-20 05:27:21
+ * Problem URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_h
+ * Result: WA
+ * Execution Time: 451 ms
+ */
+
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      int h,w;
+      cin>>h>>w;
+      vector<vector<int>> p(1509,vector<int>(1509)),r(1509,vector<int>(1509));
+      rep1(i,1,h+1) rep1(j,1,w+1) cin>>p[i][j];
+    
+      rep1(i,1,h+1) rep1(j,1,w+1){
+        r[i][j]=r[i][j-1]+p[i][j];
+      }
+      rep1(i,1,w+1) rep1(j,1,h+1){
+        r[i][j]+=r[i-1][j];
+      }
+
+
+      int q;
+      cin>>q;
+      rep(i,q){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        //a--,b--,c--,d--;
+        cout<< r[c][d]+r[a-1][b-1]-r[a-1][d]-r[c][b-1] << endl;
+      }
+    }

3e51b1dd56ecdbae3525bca114c79d8b31320882
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-09-26 02:57:17 +0000

    [AC] SubmissionID: 35171151 at 2022-09-26 02:57:17 (1664161037)

---
+++ abc226/abc226_a_35171151_AC.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc226/submissions/35171151
+ * Submitted at: 2022-09-26 02:57:17
+ * Problem URL: https://atcoder.jp/contests/abc226/tasks/abc226_a
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      double n;
+      cin>>n;
+      cout<<round(n)<<endl;
+    }

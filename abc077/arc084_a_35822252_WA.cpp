32d528e233e8be6964137c43b044af9b3732ef67
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-10-21 02:52:37 +0000

    [WA] SubmissionID: 35822252 at 2022-10-21 02:52:37 (1666320757)

---
+++ abc077/arc084_a_35822252_WA.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc077/submissions/35822252
+ * Submitted at: 2022-10-21 02:52:37
+ * Problem URL: https://atcoder.jp/contests/abc077/tasks/arc084_a
+ * Result: WA
+ * Execution Time: 120 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      int n,ans=0;
+      cin>>n;
+      vector<int> a(n),b(n),c(n);
+      rep(i,n) cin>>a[i];
+      rep(i,n) cin>>b[i];
+      rep(i,n) cin>>c[i];
+      sort(all(a));
+      sort(all(c));
+      rep(i,n){
+        int s=lower_bound(all(a),b[i])-a.begin();
+        int t=c.end()-lower_bound(all(c),b[i]+1);
+        ans+=t*s;
+        
+      }
+      cout<< ans << endl;
+
+    }

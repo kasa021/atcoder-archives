c4749cb84ab46660e0df28d998168385031aedca
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-10-22 10:43:39 +0000

    [WA] SubmissionID: 35859775 at 2022-10-22 10:43:39 (1666435419)

---
+++ abc220/abc220_b_35859775_WA.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc220/submissions/35859775
+ * Submitted at: 2022-10-22 10:43:39
+ * Problem URL: https://atcoder.jp/contests/abc220/tasks/abc220_b
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      int k,aa=0,bb=0;
+      string a,b;
+      cin>>k>>a>>b;
+      int kk=1;
+      for(int i=a.size()-1;i>=0;i--){
+        aa+=(a[i]-'0')*kk;
+        kk*=k;
+      }
+      //cout<< aa << endl;
+      kk=1;
+      for(int i=b.size()-1;i>=0;i--){
+        bb+=(b[i]-'0')*kk;
+        kk*=k;
+      }
+      //cout<< bb << endl;
+      cout<< aa*bb << endl;
+    }

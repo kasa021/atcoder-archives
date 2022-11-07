291bf69d2d6321242e75e7823d99e67fbe1ac427
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-11-07 12:12:20 +0000

    [AC] SubmissionID: 36306587 at 2022-11-07 12:12:20 (1667823140)

---
+++ abc159/abc159_d_36306587_AC.cpp
@@ -0,0 +1,48 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc159/submissions/36306587
+ * Submitted at: 2022-11-07 12:12:20
+ * Problem URL: https://atcoder.jp/contests/abc159/tasks/abc159_d
+ * Result: AC
+ * Execution Time: 532 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      ll n;
+      cin>>n;
+      vector<ll> a(n);
+      map<ll,ll> num;
+      rep(i,n) {
+        cin>>a[i];
+        num[a[i]]++;
+      }
+
+        ll ans=0;
+        for(auto [p,q]:num){
+            ans+=(q*(q-1))/2;
+        }
+        rep(i,n){
+            ll now1,now2;
+            now1=((num[a[i]]-1)*(num[a[i]]-2))/2;
+            now2=((num[a[i]])*(num[a[i]]-1))/2;
+            cout<< ans-now2+now1 << endl;
+        }
+      
+   }

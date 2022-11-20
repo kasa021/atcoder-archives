89441ba2d973d5f4e4aafc67084999ac8aaf7ba0
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-11-20 09:43:55 +0000

    [AC] SubmissionID: 36666680 at 2022-11-20 09:43:55 (1668937435)

---
+++ abc064/abc064_c_36666680_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc064/submissions/36666680
+ * Submitted at: 2022-11-20 09:43:55
+ * Problem URL: https://atcoder.jp/contests/abc064/tasks/abc064_c
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      int n,m=0,M=0;
+      cin>>n;
+      vector<int> a(n),cnt(9);
+      rep(i,n){
+        cin>>a[i];
+        if(a[i]/400>=8){
+            cnt[8]++;
+        }else cnt[a[i]/400]++;
+      }
+      rep(i,8){
+        if(cnt[i]>0) m++,M++;
+      }
+      M+=cnt[8];
+      if(m==0) m++;
+      cout<< m<<" "<<M << endl;
+    }

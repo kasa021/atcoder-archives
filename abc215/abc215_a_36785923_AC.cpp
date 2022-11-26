f22027a42e38c74eaf6fbca48d777ad99b3a59de
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-11-26 07:11:56 +0000

    [AC] SubmissionID: 36785923 at 2022-11-26 07:11:56 (1669446716)

---
+++ abc215/abc215_a_36785923_AC.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc215/submissions/36785923
+ * Submitted at: 2022-11-26 07:11:56
+ * Problem URL: https://atcoder.jp/contests/abc215/tasks/abc215_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define graph vector<vector<int>>
+#define all(vec) vec.begin(), vec.end()
+using ll = long long ;
+typedef pair<int, int> P;
+typedef tuple<string, ll, ll> T;
+
+
+int main() {
+    string s,a="Hello,World!";
+    cin>>s;
+    if(s==a) cout<< "AC" << endl;
+    else cout<< "WA" << endl;
+}
+   

22119eb5bbc2b87da74c1e92aea9ad93e2acaa07
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-11-26 06:51:29 +0000

    [AC] SubmissionID: 36785518 at 2022-11-26 06:51:29 (1669445489)

---
+++ abc200/abc200_a_36785518_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc200/submissions/36785518
+ * Submitted at: 2022-11-26 06:51:29
+ * Problem URL: https://atcoder.jp/contests/abc200/tasks/abc200_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define graph vector<vector<int>>
+#define all(vec) vec.begin(), vec.end()
+using ll = long long ;
+typedef pair<int, int> P;
+typedef tuple<string, ll, ll> T;
+
+
+int main() {
+    int n;
+    cin>>n;
+    n+=99;
+    cout<< n/100 << endl;
+}
+   
+
+   

6b6dc4a64759c14b153838a3392a1d6a0438126b
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-11-26 07:17:22 +0000

    [AC] SubmissionID: 36786041 at 2022-11-26 07:17:22 (1669447042)

---
+++ abc174/abc174_a_36786041_AC.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc174/submissions/36786041
+ * Submitted at: 2022-11-26 07:17:22
+ * Problem URL: https://atcoder.jp/contests/abc174/tasks/abc174_a
+ * Result: AC
+ * Execution Time: 14 ms
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define graph vector<vector<int>>
+#define all(vec) vec.begin(), vec.end()
+using ll = long long ;
+typedef pair<int, int> P;
+typedef tuple<string, ll, ll> T;
+
+
+int main() {
+    int x;
+    cin>>x;
+    if(x>=30) cout<< "Yes" << endl;
+    else cout<< "No" << endl;
+}
+   

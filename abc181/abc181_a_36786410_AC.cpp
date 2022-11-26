20794f2143f90de8c0049abd43c12c32d64771ed
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-11-26 07:34:04 +0000

    [AC] SubmissionID: 36786410 at 2022-11-26 07:34:04 (1669448044)

---
+++ abc181/abc181_a_36786410_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc181/submissions/36786410
+ * Submitted at: 2022-11-26 07:34:04
+ * Problem URL: https://atcoder.jp/contests/abc181/tasks/abc181_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define graph vector<vector<int>>
+#define all(vec) vec.begin(), vec.end()
+using ll = long long ;
+typedef pair<int, int> P;
+typedef tuple<string, ll, ll> T;
+
+
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==1){
+        cout<< "Black" << endl;
+    }else cout<< "White" << endl;
+}
+   

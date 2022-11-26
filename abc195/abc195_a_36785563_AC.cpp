dbc28d043a647f9e98ed5de708f56c076d15dff9
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-11-26 06:53:25 +0000

    [AC] SubmissionID: 36785563 at 2022-11-26 06:53:25 (1669445605)

---
+++ abc195/abc195_a_36785563_AC.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc195/submissions/36785563
+ * Submitted at: 2022-11-26 06:53:25
+ * Problem URL: https://atcoder.jp/contests/abc195/tasks/abc195_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define graph vector<vector<int>>
+#define all(vec) vec.begin(), vec.end()
+using ll = long long ;
+typedef pair<int, int> P;
+typedef tuple<string, ll, ll> T;
+
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    if(m%n==0)cout<< "Yes" << endl;
+    else cout<< "No" << endl;
+}
+   

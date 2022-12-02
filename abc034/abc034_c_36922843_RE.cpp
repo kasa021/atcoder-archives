44dd8c75225dc5b7946224de19b6d7143cfe12c9
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-12-02 04:09:18 +0000

    [RE] SubmissionID: 36922843 at 2022-12-02 04:09:18 (1669954158)

---
+++ abc034/abc034_c_36922843_RE.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc034/submissions/36922843
+ * Submitted at: 2022-12-02 04:09:18
+ * Problem URL: https://atcoder.jp/contests/abc034/tasks/abc034_c
+ * Result: RE
+ * Execution Time: 107 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+typedef modint1000000007 mint;
+//cout<<x.val()<<endl;
+int main(){
+      int n,m;
+      cin>>n>>m;
+      n--,m--;
+      mint ans=1;
+      //mint::set_mod(ans);
+      rep(i,n+m) ans*=i;
+      rep(i,n) ans/=i;
+      rep(i,m) ans/=i;
+      cout<< ans.val() << endl;
+    }

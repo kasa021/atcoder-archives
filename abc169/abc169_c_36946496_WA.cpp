17f96d54d1e8cc7cd922d0c91f83cf99e5e69677
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-12-03 11:20:08 +0000

    [WA] SubmissionID: 36946496 at 2022-12-03 11:20:08 (1670066408)

---
+++ abc169/abc169_c_36946496_WA.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc169/submissions/36946496
+ * Submitted at: 2022-12-03 11:20:08
+ * Problem URL: https://atcoder.jp/contests/abc169/tasks/abc169_c
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+    long double a,b;
+    cin>>a>>b;
+    cout<< floor(a*b) << endl;
+    }

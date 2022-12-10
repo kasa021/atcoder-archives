d62ae40e29cc4617cf41130ec005fbc943137787
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-12-10 12:01:00 +0000

    [AC] SubmissionID: 37136286 at 2022-12-10 12:01:00 (1670673660)

---
+++ abc281/abc281_a_37136286_AC.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc281/submissions/37136286
+ * Submitted at: 2022-12-10 12:01:00
+ * Problem URL: https://atcoder.jp/contests/abc281/tasks/abc281_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+//typedef modint1000000007 mint;
+int main(){
+      int n;
+      cin>>n;
+      for(int i=n;i>=0;i--) cout<< i << endl;
+    }

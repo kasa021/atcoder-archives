6bc6c9ba2f1c19d25bb4eee81d05e2465221dfc2
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-12-16 16:59:21 +0000

    [WA] SubmissionID: 37307832 at 2022-12-16 16:59:21 (1671209961)

---
+++ abc076/abc076_c_37307832_WA.cpp
@@ -0,0 +1,58 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc076/submissions/37307832
+ * Submitted at: 2022-12-16 16:59:21
+ * Problem URL: https://atcoder.jp/contests/abc076/tasks/abc076_c
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+int main(){
+      string s,t,ans="";
+      int idx=-1;
+      cin>>s>>t;
+      rep(i,(int)s.size()){
+        bool f=true;
+        rep(j,(int)t.size()){
+            if(j+i>(int)s.size()){
+                f=false;
+                continue;
+            }else {
+                if(s[i+j]!='?' && s[i+j]!=t[j]) f=false;
+            }
+        }
+        if(f){
+            idx=i;
+            //cout<< i << endl;
+        }
+      }
+      if(idx==-1) cout<< "UNRESTORABLE" << endl;
+      else {
+        int now=0;
+        while(now<idx){
+            if(s[now]=='?'){
+                ans+='a';
+            }else ans+=s[now];
+            now++;
+        }
+        ans+=t;
+        cout<< ans << endl;
+      }
+    }

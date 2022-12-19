36cde0fa07efe16917647f2c6517d829ee738099
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-12-19 14:05:00 +0000

    [WA] SubmissionID: 37396825 at 2022-12-19 14:05:00 (1671458700)

---
+++ abc238/abc238_d_37396825_WA.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc238/submissions/37396825
+ * Submitted at: 2022-12-19 14:05:00
+ * Problem URL: https://atcoder.jp/contests/abc238/tasks/abc238_d
+ * Result: WA
+ * Execution Time: 244 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+//typedef modint1000000007 mint;
+
+
+int main(){
+    ll t;
+    cin>>t;
+    rep(i,t){
+        ll a,b,x;
+        cin>>a>>b;
+        x=b-2*a;
+        //cout<< x << endl;
+        if(x<0){
+            cout<< "No" << endl;
+        }else if(x==0){
+            cout<< "Yes" << endl;
+        }else {
+            if(x%2){
+                if((x & a) == 0 || ((x/2 & a)==0 &&((x+1)/2 & a)==0)) cout<< "Yes" << endl;
+                else cout<< "No" << endl;
+            }else {
+                if((x & a) == 0 || (x/2 & a) == 0) cout<< "Yes" << endl;
+                else cout<< "No" << endl;
+                //cout<< (x & a) << endl;
+            }
+        }
+    }
+    }

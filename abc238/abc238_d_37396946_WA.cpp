a8a603ac7000e35141cc94f6352a07da9324a746
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-12-19 14:12:04 +0000

    [WA] SubmissionID: 37396946 at 2022-12-19 14:12:04 (1671459124)

---
+++ abc238/abc238_d_37396946_WA.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc238/submissions/37396946
+ * Submitted at: 2022-12-19 14:12:04
+ * Problem URL: https://atcoder.jp/contests/abc238/tasks/abc238_d
+ * Result: WA
+ * Execution Time: 224 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+//typedef modint1000000007 mint;
+
+
+int main(){
+    ll t;
+    cin>>t;
+    rep(i,t){
+        ll a,b,x;
+        cin>>a>>b;
+        x=b-2*a;
+        //cout<< x << endl;
+        if(x>=0){
+            if((x & a) == 0)cout<< "Yes" << endl;
+        }else cout<< "No" << endl;
+    }
+    }

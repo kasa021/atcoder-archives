e138f59ce19b2b1dd8faab1027408eddfd4bb1bb
kasa021 <109140298+kasa021@users.noreply.github.com>
2022-12-27 05:57:17 +0000

    [AC] SubmissionID: 37576103 at 2022-12-27 05:57:17 (1672120637)

---
+++ abc283/abc283_d_37576103_AC.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc283/submissions/37576103
+ * Submitted at: 2022-12-27 05:57:17
+ * Problem URL: https://atcoder.jp/contests/abc283/tasks/abc283_d
+ * Result: AC
+ * Execution Time: 19 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+//typedef modint1000000007 mint;
+int main(){
+      string s;
+      cin>>s;
+      stack<int> left;
+      vector<int> mp(30,-1);
+      rep(i,(int)s.size()){
+        if(s[i]=='('){
+            left.push(i);
+        }else if(s[i]==')'){
+            int j=left.top();
+            left.pop();
+            rep(k,30){
+                if(mp[k]>=j && mp[k]<=i) mp[k]=-1;
+            }
+        }else {
+            if(mp[s[i]-'a']!=-1){
+                cout<< "No" << endl;
+                return 0;
+            }else {
+                mp[s[i]-'a']=i;
+            }
+        }
+      }
+      cout<< "Yes" << endl;
+    }

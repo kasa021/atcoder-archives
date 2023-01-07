c100a4db92112d0f1cb3674e2b3a6bc34d0118fc
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-01-07 12:20:40 +0000

    [AC] SubmissionID: 37814316 at 2023-01-07 12:20:40 (1673094040)

---
+++ abc284/abc284_c_37814316_AC.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc284/submissions/37814316
+ * Submitted at: 2023-01-07 12:20:40
+ * Problem URL: https://atcoder.jp/contests/abc284/tasks/abc284_c
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+// typedef modint1000000007 mint;
+
+
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    dsu uf(n+9);
+    // if(m==0){
+    //     cout<< n << endl;
+    //     return 0;
+    // }
+    rep(i,m){
+        int a,b;
+        cin>>a>>b;
+        uf.merge(a,b);
+    }
+    int ans=0;
+    rep(i,n){
+        if(i+1 == uf.leader(i+1)) ans++;
+    }
+    cout<< ans<< endl;
+}

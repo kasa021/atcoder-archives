3a79f3b121f5f0f6e8f2ea4d04b136867d367fc3
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-01-14 02:45:35 +0000

    [AC] SubmissionID: 37990927 at 2023-01-14 02:45:35 (1673664335)

---
+++ abc151/abc151_a_37990927_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc151/submissions/37990927
+ * Submitted at: 2023-01-14 02:45:35
+ * Problem URL: https://atcoder.jp/contests/abc151/tasks/abc151_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char a;
+  cin >> a;
+  cout << (char)(a + 1) << endl;
+}

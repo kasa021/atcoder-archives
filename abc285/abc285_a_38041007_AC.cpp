97596f034922648569a6d3f0d8f53078dc21d2a4
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-01-15 12:00:57 +0000

    [AC] SubmissionID: 38041007 at 2023-01-15 12:00:57 (1673784057)

---
+++ abc285/abc285_a_38041007_AC.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc285/submissions/38041007
+ * Submitted at: 2023-01-15 12:00:57
+ * Problem URL: https://atcoder.jp/contests/abc285/tasks/abc285_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+//typedef modint1000000007 mint;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a==(b/2)){
+        cout<< "Yes" << endl;
+    }else cout<< "No" << endl;
+    }

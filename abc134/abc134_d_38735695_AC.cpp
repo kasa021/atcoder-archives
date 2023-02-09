1a53811a03d651b6cd54a3d3ac451213b90a309b
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-02-09 11:05:17 +0000

    [AC] SubmissionID: 38735695 at 2023-02-09 11:05:17 (1675940717)

---
+++ abc134/abc134_d_38735695_AC.cpp
@@ -0,0 +1,59 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc134/submissions/38735695
+ * Submitted at: 2023-02-09 11:05:17
+ * Problem URL: https://atcoder.jp/contests/abc134/tasks/abc134_d
+ * Result: AC
+ * Execution Time: 172 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); ++i)
+#define rep1(i,s,n) for(int i=s;i<n;i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int,int>;
+using T = tuple<int,int,int>;
+using graph=vector<vector<int>>;
+
+vector<int> enum_div(int n){
+    vector<int> res;
+    res.push_back(1);
+    for(int i = 2 ; i*i <= n ; ++i){
+        if(n%i == 0){
+            res.push_back(i);
+            if(i*i != n){
+                res.push_back(n/i);
+           }
+       }
+   }
+return res;
+}
+
+int main(){
+      int n;
+      cin>>n;
+      vector<int> a(n),cnt(n),ans;
+      rep(i,n) cin>>a[i];
+      for(int i=n-1;i>=0;i--){
+        if((a[i]==1&&cnt[i]%2==0) || (a[i]==0&&cnt[i]%2==1)){
+                cnt[i]++;
+                ans.push_back(i);
+                vector<int> res=enum_div(i+1);
+                for(auto v:res){
+                    cnt[v-1]++;
+                }
+        }
+      }
+      cout<< ans.size() << endl;
+      for(auto v:ans) cout<<v+1<<" ";
+      cout<<endl;
+    }

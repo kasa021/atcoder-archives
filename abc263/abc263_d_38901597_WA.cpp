670f90fdcaa7c00ccf4adaa1213f14014889d8b7
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-02-15 07:21:41 +0000

    [WA] SubmissionID: 38901597 at 2023-02-15 07:21:41 (1676445701)

---
+++ abc263/abc263_d_38901597_WA.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc263/submissions/38901597
+ * Submitted at: 2023-02-15 07:21:41
+ * Problem URL: https://atcoder.jp/contests/abc263/tasks/abc263_d
+ * Result: WA
+ * Execution Time: 76 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+
+int main()
+{
+    ll n,l,r;
+    cin>>n>>l>>r;
+    vector<ll> a(n);
+    rep(i,n) cin>>a[i];
+    vector<ll> f(n+9,0),g(n+9,0);
+    rep(i,n){
+        f[i+1]=min(f[i]+a[i],l*(i+1));
+        g[i+1]=min(g[i]+a[n-(i+1)],r*(i+1));
+       // cout<<f[i]<<" "<<g[i]<<endl;
+    }
+    //cout<< f[n]<<" "<<g[n] << endl;
+    ll ans=1e18;
+    rep(i,n) ans=min(ans,f[i]+g[n-i]);
+    cout<< ans << endl;
+}

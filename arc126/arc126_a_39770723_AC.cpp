abe5ce7b40201c00aa1ee431edb0d8fc44e4a101
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-16 12:22:51 +0000

    [AC] SubmissionID: 39770723 at 2023-03-16 12:22:51 (1678969371)

---
+++ arc126/arc126_a_39770723_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc126/submissions/39770723
+ * Submitted at: 2023-03-16 12:22:51
+ * Problem URL: https://atcoder.jp/contests/arc126/tasks/arc126_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+// code not found

3fb68c0a6d073d1d37e2bf1b85735dd45e105a5c
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-16 04:31:19 +0000

    [RE] SubmissionID: 39762045 at 2023-03-16 04:31:19 (1678941079)

---
+++ arc130/arc130_b_39762045_RE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc130/submissions/39762045
+ * Submitted at: 2023-03-16 04:31:19
+ * Problem URL: https://atcoder.jp/contests/arc130/tasks/arc130_b
+ * Result: RE
+ * Execution Time: 395 ms
+ */
+
+// code not found

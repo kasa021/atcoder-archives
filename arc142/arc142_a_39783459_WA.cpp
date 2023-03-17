3e10a982a9de2c063699b6202fc860c361af7255
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-17 05:48:14 +0000

    [WA] SubmissionID: 39783459 at 2023-03-17 05:48:14 (1679032094)

---
+++ arc142/arc142_a_39783459_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc142/submissions/39783459
+ * Submitted at: 2023-03-17 05:48:14
+ * Problem URL: https://atcoder.jp/contests/arc142/tasks/arc142_a
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+// code not found

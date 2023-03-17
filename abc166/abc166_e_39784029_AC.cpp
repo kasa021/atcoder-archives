aa69a91b1ebbf461f2ba043370c2c6b5d8c7481c
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-17 06:15:22 +0000

    [AC] SubmissionID: 39784029 at 2023-03-17 06:15:22 (1679033722)

---
+++ abc166/abc166_e_39784029_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc166/submissions/39784029
+ * Submitted at: 2023-03-17 06:15:22
+ * Problem URL: https://atcoder.jp/contests/abc166/tasks/abc166_e
+ * Result: AC
+ * Execution Time: 197 ms
+ */
+
+// code not found

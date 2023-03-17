f30c6374a71615f9c2b4cac27221bb09d83a4a71
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-17 05:49:20 +0000

    [AC] SubmissionID: 39783484 at 2023-03-17 05:49:20 (1679032160)

---
+++ arc142/arc142_a_39783484_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc142/submissions/39783484
+ * Submitted at: 2023-03-17 05:49:20
+ * Problem URL: https://atcoder.jp/contests/arc142/tasks/arc142_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+// code not found

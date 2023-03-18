e74136d04894878f88ecc5bbd53cf889c17a59ff
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-18 13:58:20 +0000

    [AC] SubmissionID: 39821984 at 2023-03-18 13:58:20 (1679147900)

---
+++ arc113/arc113_b_39821984_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc113/submissions/39821984
+ * Submitted at: 2023-03-18 13:58:20
+ * Problem URL: https://atcoder.jp/contests/arc113/tasks/arc113_b
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+// code not found

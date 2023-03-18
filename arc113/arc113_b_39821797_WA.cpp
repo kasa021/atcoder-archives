863c2388d69f608023c9142bdbf0b9fe49f5c382
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-18 13:50:15 +0000

    [WA] SubmissionID: 39821797 at 2023-03-18 13:50:15 (1679147415)

---
+++ arc113/arc113_b_39821797_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc113/submissions/39821797
+ * Submitted at: 2023-03-18 13:50:15
+ * Problem URL: https://atcoder.jp/contests/arc113/tasks/arc113_b
+ * Result: WA
+ * Execution Time: 5 ms
+ */
+
+// code not found

176943642e1c7b5e85f936423fef3eb33f1b47bf
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-18 06:05:54 +0000

    [AC] SubmissionID: 39808812 at 2023-03-18 06:05:54 (1679119554)

---
+++ abc260/abc260_d_39808812_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc260/submissions/39808812
+ * Submitted at: 2023-03-18 06:05:54
+ * Problem URL: https://atcoder.jp/contests/abc260/tasks/abc260_d
+ * Result: AC
+ * Execution Time: 121 ms
+ */
+
+// code not found

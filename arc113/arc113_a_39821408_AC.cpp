27e2efa05757cfe159f33319a16048d6f00fcd50
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-18 13:34:30 +0000

    [AC] SubmissionID: 39821408 at 2023-03-18 13:34:30 (1679146470)

---
+++ arc113/arc113_a_39821408_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc113/submissions/39821408
+ * Submitted at: 2023-03-18 13:34:30
+ * Problem URL: https://atcoder.jp/contests/arc113/tasks/arc113_a
+ * Result: AC
+ * Execution Time: 536 ms
+ */
+
+// code not found

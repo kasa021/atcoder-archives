7c1bcb3c5abd019251e17654cebd5a009f0d7852
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 12:58:53 +0000

    [AC] SubmissionID: 39870543 at 2023-03-19 12:58:53 (1679230733)

---
+++ abc294/abc294_d_39870543_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc294/submissions/39870543
+ * Submitted at: 2023-03-19 12:58:53
+ * Problem URL: https://atcoder.jp/contests/abc294/tasks/abc294_d
+ * Result: AC
+ * Execution Time: 719 ms
+ */
+
+// code not found

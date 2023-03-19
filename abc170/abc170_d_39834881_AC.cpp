eca59a4015bedc3bb64a40427a9124f597f2ec1d
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 08:01:42 +0000

    [AC] SubmissionID: 39834881 at 2023-03-19 08:01:42 (1679212902)

---
+++ abc170/abc170_d_39834881_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc170/submissions/39834881
+ * Submitted at: 2023-03-19 08:01:42
+ * Problem URL: https://atcoder.jp/contests/abc170/tasks/abc170_d
+ * Result: AC
+ * Execution Time: 123 ms
+ */
+
+// code not found

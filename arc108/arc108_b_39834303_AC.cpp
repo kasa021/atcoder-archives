236e8a6d976aadb06defc56bf5fe3ad8602edaac
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 07:35:05 +0000

    [AC] SubmissionID: 39834303 at 2023-03-19 07:35:05 (1679211305)

---
+++ arc108/arc108_b_39834303_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc108/submissions/39834303
+ * Submitted at: 2023-03-19 07:35:05
+ * Problem URL: https://atcoder.jp/contests/arc108/tasks/arc108_b
+ * Result: AC
+ * Execution Time: 17 ms
+ */
+
+// code not found

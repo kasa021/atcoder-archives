e1fc45011a58ecdca7638c822a1c8d81baff2625
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 12:49:04 +0000

    [RE] SubmissionID: 39867948 at 2023-03-19 12:49:04 (1679230144)

---
+++ abc294/abc294_d_39867948_RE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc294/submissions/39867948
+ * Submitted at: 2023-03-19 12:49:04
+ * Problem URL: https://atcoder.jp/contests/abc294/tasks/abc294_d
+ * Result: RE
+ * Execution Time: 183 ms
+ */
+
+// code not found

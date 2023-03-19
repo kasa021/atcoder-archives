51fadbdc315052377a74f74ed3161e7a24380f03
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 12:17:18 +0000

    [TLE] SubmissionID: 39856577 at 2023-03-19 12:17:18 (1679228238)

---
+++ abc294/abc294_c_39856577_TLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc294/submissions/39856577
+ * Submitted at: 2023-03-19 12:17:18
+ * Problem URL: https://atcoder.jp/contests/abc294/tasks/abc294_c
+ * Result: TLE
+ * Execution Time: 2206 ms
+ */
+
+// code not found

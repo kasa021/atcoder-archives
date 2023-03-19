cbb0faca11c434590dc48854624edf5d2099dcbe
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 13:33:08 +0000

    [AC] SubmissionID: 39878523 at 2023-03-19 13:33:08 (1679232788)

---
+++ abc294/abc294_e_39878523_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc294/submissions/39878523
+ * Submitted at: 2023-03-19 13:33:08
+ * Problem URL: https://atcoder.jp/contests/abc294/tasks/abc294_e
+ * Result: AC
+ * Execution Time: 113 ms
+ */
+
+// code not found

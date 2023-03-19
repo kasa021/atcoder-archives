11a0f2aaf9b8029260182ea2eab590459362cf41
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 07:47:57 +0000

    [WA] SubmissionID: 39834599 at 2023-03-19 07:47:57 (1679212077)

---
+++ abc170/abc170_d_39834599_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc170/submissions/39834599
+ * Submitted at: 2023-03-19 07:47:57
+ * Problem URL: https://atcoder.jp/contests/abc170/tasks/abc170_d
+ * Result: WA
+ * Execution Time: 586 ms
+ */
+
+// code not found

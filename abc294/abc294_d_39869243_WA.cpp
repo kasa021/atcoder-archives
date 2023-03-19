45a8a90a1c5f8a4fbaf2d2b790a105cc81ac4183
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-19 12:53:55 +0000

    [WA] SubmissionID: 39869243 at 2023-03-19 12:53:55 (1679230435)

---
+++ abc294/abc294_d_39869243_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc294/submissions/39869243
+ * Submitted at: 2023-03-19 12:53:55
+ * Problem URL: https://atcoder.jp/contests/abc294/tasks/abc294_d
+ * Result: WA
+ * Execution Time: 715 ms
+ */
+
+// code not found

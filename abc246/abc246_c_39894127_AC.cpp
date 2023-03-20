843a9cf313063baecc27486d586eec003a9b525b
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-20 05:00:28 +0000

    [AC] SubmissionID: 39894127 at 2023-03-20 05:00:28 (1679288428)

---
+++ abc246/abc246_c_39894127_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc246/submissions/39894127
+ * Submitted at: 2023-03-20 05:00:28
+ * Problem URL: https://atcoder.jp/contests/abc246/tasks/abc246_c
+ * Result: AC
+ * Execution Time: 79 ms
+ */
+
+// code not found

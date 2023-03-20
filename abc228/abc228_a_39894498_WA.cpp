96baa3e948141f62e1363e7620da48317cf6bdca
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-20 05:23:05 +0000

    [WA] SubmissionID: 39894498 at 2023-03-20 05:23:05 (1679289785)

---
+++ abc228/abc228_a_39894498_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc228/submissions/39894498
+ * Submitted at: 2023-03-20 05:23:05
+ * Problem URL: https://atcoder.jp/contests/abc228/tasks/abc228_a
+ * Result: WA
+ * Execution Time: 10 ms
+ */
+
+// code not found

db9361c0667417b38adff73404867dd7f572c617
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-21 06:28:39 +0000

    [AC] SubmissionID: 39919327 at 2023-03-21 06:28:39 (1679380119)

---
+++ abc221/abc221_c_39919327_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc221/submissions/39919327
+ * Submitted at: 2023-03-21 06:28:39
+ * Problem URL: https://atcoder.jp/contests/abc221/tasks/abc221_c
+ * Result: AC
+ * Execution Time: 194 ms
+ */
+
+// code not found

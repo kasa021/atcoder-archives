303389a238faeadadeda97417ca01245b4f21e60
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-21 06:31:01 +0000

    [AC] SubmissionID: 39919379 at 2023-03-21 06:31:01 (1679380261)

---
+++ abc217/abc217_b_39919379_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc217/submissions/39919379
+ * Submitted at: 2023-03-21 06:31:01
+ * Problem URL: https://atcoder.jp/contests/abc217/tasks/abc217_b
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+// code not found

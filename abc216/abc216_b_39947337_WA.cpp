2e8c24f5c4451894d17619e89c633e1ddf2d7c3f
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-22 11:09:03 +0000

    [WA] SubmissionID: 39947337 at 2023-03-22 11:09:03 (1679483343)

---
+++ abc216/abc216_b_39947337_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc216/submissions/39947337
+ * Submitted at: 2023-03-22 11:09:03
+ * Problem URL: https://atcoder.jp/contests/abc216/tasks/abc216_b
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+// code not found

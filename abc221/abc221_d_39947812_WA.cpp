1d1d1ddd95d3d94d8fc8ddc70697fa8145917263
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-22 11:25:18 +0000

    [WA] SubmissionID: 39947812 at 2023-03-22 11:25:18 (1679484318)

---
+++ abc221/abc221_d_39947812_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc221/submissions/39947812
+ * Submitted at: 2023-03-22 11:25:18
+ * Problem URL: https://atcoder.jp/contests/abc221/tasks/abc221_d
+ * Result: WA
+ * Execution Time: 539 ms
+ */
+
+// code not found

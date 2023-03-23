fe319e4a35b536260dabe6c61bffed80b3838f2f
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-23 08:02:20 +0000

    [AC] SubmissionID: 39965030 at 2023-03-23 08:02:20 (1679558540)

---
+++ abc213/abc213_a_39965030_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc213/submissions/39965030
+ * Submitted at: 2023-03-23 08:02:20
+ * Problem URL: https://atcoder.jp/contests/abc213/tasks/abc213_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+// code not found

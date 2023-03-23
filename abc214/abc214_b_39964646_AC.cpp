88171369c5d6c19f152fdd0679b0e4e426ab57cc
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-23 07:44:01 +0000

    [AC] SubmissionID: 39964646 at 2023-03-23 07:44:01 (1679557441)

---
+++ abc214/abc214_b_39964646_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc214/submissions/39964646
+ * Submitted at: 2023-03-23 07:44:01
+ * Problem URL: https://atcoder.jp/contests/abc214/tasks/abc214_b
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+// code not found

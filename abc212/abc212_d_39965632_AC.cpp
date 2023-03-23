96dd8e8a8789af95071080bc1886fb39b32f2648
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-23 08:35:44 +0000

    [AC] SubmissionID: 39965632 at 2023-03-23 08:35:44 (1679560544)

---
+++ abc212/abc212_d_39965632_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc212/submissions/39965632
+ * Submitted at: 2023-03-23 08:35:44
+ * Problem URL: https://atcoder.jp/contests/abc212/tasks/abc212_d
+ * Result: AC
+ * Execution Time: 210 ms
+ */
+
+// code not found

90906378d1c10d412ad4815d01a079dd3d4b422e
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-23 08:53:11 +0000

    [AC] SubmissionID: 39966011 at 2023-03-23 08:53:11 (1679561591)

---
+++ abc138/abc138_d_39966011_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc138/submissions/39966011
+ * Submitted at: 2023-03-23 08:53:11
+ * Problem URL: https://atcoder.jp/contests/abc138/tasks/abc138_d
+ * Result: AC
+ * Execution Time: 227 ms
+ */
+
+// code not found

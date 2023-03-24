9a973c274c1185276609c4888bce90b3b9da9304
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-24 04:35:48 +0000

    [AC] SubmissionID: 39981900 at 2023-03-24 04:35:48 (1679632548)

---
+++ abc210/abc210_b_39981900_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc210/submissions/39981900
+ * Submitted at: 2023-03-24 04:35:48
+ * Problem URL: https://atcoder.jp/contests/abc210/tasks/abc210_b
+ * Result: AC
+ * Execution Time: 19 ms
+ */
+
+// code not found

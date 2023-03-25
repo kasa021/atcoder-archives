1ccac029ade2d83073b34df68fabd46fadc2b344
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-25 07:06:10 +0000

    [AC] SubmissionID: 40011074 at 2023-03-25 07:06:10 (1679727970)

---
+++ abc178/abc178_e_40011074_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc178/submissions/40011074
+ * Submitted at: 2023-03-25 07:06:10
+ * Problem URL: https://atcoder.jp/contests/abc178/tasks/abc178_e
+ * Result: AC
+ * Execution Time: 141 ms
+ */
+
+// code not found

308b36e73b73f381747a170dc8cbc7c76c219f77
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-26 15:41:23 +0000

    [WA] SubmissionID: 40085858 at 2023-03-26 15:41:23 (1679845283)

---
+++ agc006/agc006_a_40085858_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/agc006/submissions/40085858
+ * Submitted at: 2023-03-26 15:41:23
+ * Problem URL: https://atcoder.jp/contests/agc006/tasks/agc006_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+// code not found

3bc5ec3acf47afd4bb32f62363855846fb07c528
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-26 15:48:53 +0000

    [AC] SubmissionID: 40085963 at 2023-03-26 15:48:53 (1679845733)

---
+++ agc006/agc006_a_40085963_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/agc006/submissions/40085963
+ * Submitted at: 2023-03-26 15:48:53
+ * Problem URL: https://atcoder.jp/contests/agc006/tasks/agc006_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+// code not found

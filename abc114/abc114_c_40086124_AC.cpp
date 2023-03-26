584c45f99b3044e1f00845e7128ade34263950a3
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-26 16:02:32 +0000

    [AC] SubmissionID: 40086124 at 2023-03-26 16:02:32 (1679846552)

---
+++ abc114/abc114_c_40086124_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc114/submissions/40086124
+ * Submitted at: 2023-03-26 16:02:32
+ * Problem URL: https://atcoder.jp/contests/abc114/tasks/abc114_c
+ * Result: AC
+ * Execution Time: 16 ms
+ */
+
+// code not found

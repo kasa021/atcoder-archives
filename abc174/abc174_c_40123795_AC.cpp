06faeb02d705f43a17215c553bb8adfbd27639c3
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-28 11:05:52 +0000

    [AC] SubmissionID: 40123795 at 2023-03-28 11:05:52 (1680001552)

---
+++ abc174/abc174_c_40123795_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc174/submissions/40123795
+ * Submitted at: 2023-03-28 11:05:52
+ * Problem URL: https://atcoder.jp/contests/abc174/tasks/abc174_c
+ * Result: AC
+ * Execution Time: 19 ms
+ */
+
+// code not found

ccf4daf3a5266fb28cf287dbfdcea30b55d75c5e
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-28 06:37:26 +0000

    [RE] SubmissionID: 40118539 at 2023-03-28 06:37:26 (1679985446)

---
+++ abc216/abc216_d_40118539_RE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc216/submissions/40118539
+ * Submitted at: 2023-03-28 06:37:26
+ * Problem URL: https://atcoder.jp/contests/abc216/tasks/abc216_d
+ * Result: RE
+ * Execution Time: 874 ms
+ */
+
+// code not found

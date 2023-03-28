544441de8f3bc1a3bd9d0d5e0c77fce21a05eb16
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-28 06:10:12 +0000

    [AC] SubmissionID: 40117904 at 2023-03-28 06:10:12 (1679983812)

---
+++ abc218/abc218_c_40117904_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc218/submissions/40117904
+ * Submitted at: 2023-03-28 06:10:12
+ * Problem URL: https://atcoder.jp/contests/abc218/tasks/abc218_c
+ * Result: AC
+ * Execution Time: 11 ms
+ */
+
+// code not found

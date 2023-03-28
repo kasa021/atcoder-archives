a8070a9c0d3c02ba8697c1f56614386a56b2a726
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-28 14:18:30 +0000

    [AC] SubmissionID: 40128733 at 2023-03-28 14:18:30 (1680013110)

---
+++ abc134/abc134_d_40128733_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc134/submissions/40128733
+ * Submitted at: 2023-03-28 14:18:30
+ * Problem URL: https://atcoder.jp/contests/abc134/tasks/abc134_d
+ * Result: AC
+ * Execution Time: 167 ms
+ */
+
+// code not found

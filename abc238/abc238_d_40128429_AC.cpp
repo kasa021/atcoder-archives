8785dba6971f113d6a33caadac875c56cdd26ba2
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-28 14:05:28 +0000

    [AC] SubmissionID: 40128429 at 2023-03-28 14:05:28 (1680012328)

---
+++ abc238/abc238_d_40128429_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc238/submissions/40128429
+ * Submitted at: 2023-03-28 14:05:28
+ * Problem URL: https://atcoder.jp/contests/abc238/tasks/abc238_d
+ * Result: AC
+ * Execution Time: 244 ms
+ */
+
+// code not found

72ef5fd5ccb84be58fabfda8024f62ac1a9baf82
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-30 12:48:43 +0000

    [AC] SubmissionID: 40166953 at 2023-03-30 12:48:43 (1680180523)

---
+++ abc129/abc129_d_40166953_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc129/submissions/40166953
+ * Submitted at: 2023-03-30 12:48:43
+ * Problem URL: https://atcoder.jp/contests/abc129/tasks/abc129_d
+ * Result: AC
+ * Execution Time: 273 ms
+ */
+
+// code not found

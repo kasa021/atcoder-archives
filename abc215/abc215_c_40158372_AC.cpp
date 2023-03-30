cb3ff10ead11cd6260149c0d834e4e93ca581c37
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-30 05:05:50 +0000

    [AC] SubmissionID: 40158372 at 2023-03-30 05:05:50 (1680152750)

---
+++ abc215/abc215_c_40158372_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc215/submissions/40158372
+ * Submitted at: 2023-03-30 05:05:50
+ * Problem URL: https://atcoder.jp/contests/abc215/tasks/abc215_c
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+// code not found

95ecb61279ffef394bc70d08bc2a9651515152aa
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-30 09:44:09 +0000

    [AC] SubmissionID: 40163156 at 2023-03-30 09:44:09 (1680169449)

---
+++ abc232/abc232_c_40163156_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc232/submissions/40163156
+ * Submitted at: 2023-03-30 09:44:09
+ * Problem URL: https://atcoder.jp/contests/abc232/tasks/abc232_c
+ * Result: AC
+ * Execution Time: 29 ms
+ */
+
+// code not found

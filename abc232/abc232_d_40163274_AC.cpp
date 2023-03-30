1ba2e7d6886ac64303c9ca56d9377165ff434036
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-30 09:52:40 +0000

    [AC] SubmissionID: 40163274 at 2023-03-30 09:52:40 (1680169960)

---
+++ abc232/abc232_d_40163274_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc232/submissions/40163274
+ * Submitted at: 2023-03-30 09:52:40
+ * Problem URL: https://atcoder.jp/contests/abc232/tasks/abc232_d
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+// code not found

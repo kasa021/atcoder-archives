e00f86bbcbecbb20ab7c78601d5c0cf63be9023b
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-31 04:39:21 +0000

    [WA] SubmissionID: 40177729 at 2023-03-31 04:39:21 (1680237561)

---
+++ agc039/agc039_a_40177729_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/agc039/submissions/40177729
+ * Submitted at: 2023-03-31 04:39:21
+ * Problem URL: https://atcoder.jp/contests/agc039/tasks/agc039_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+// code not found

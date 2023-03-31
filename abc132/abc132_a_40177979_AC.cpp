91e1d3bdd7ece355a531e56baa3d073d41b78bfb
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-31 04:59:14 +0000

    [AC] SubmissionID: 40177979 at 2023-03-31 04:59:14 (1680238754)

---
+++ abc132/abc132_a_40177979_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc132/submissions/40177979
+ * Submitted at: 2023-03-31 04:59:14
+ * Problem URL: https://atcoder.jp/contests/abc132/tasks/abc132_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+// code not found

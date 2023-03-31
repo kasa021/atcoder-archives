2325ff0c391414b3071fa65a3e596613a91ee5fd
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-03-31 04:35:55 +0000

    [WA] SubmissionID: 40177677 at 2023-03-31 04:35:55 (1680237355)

---
+++ agc039/agc039_a_40177677_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/agc039/submissions/40177677
+ * Submitted at: 2023-03-31 04:35:55
+ * Problem URL: https://atcoder.jp/contests/agc039/tasks/agc039_a
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+// code not found

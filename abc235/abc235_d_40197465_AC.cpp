1db4836808468d2144b687738b7e583aa4c4db9f
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-01 04:35:07 +0000

    [AC] SubmissionID: 40197465 at 2023-04-01 04:35:07 (1680323707)

---
+++ abc235/abc235_d_40197465_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc235/submissions/40197465
+ * Submitted at: 2023-04-01 04:35:07
+ * Problem URL: https://atcoder.jp/contests/abc235/tasks/abc235_d
+ * Result: AC
+ * Execution Time: 66 ms
+ */
+
+// code not found

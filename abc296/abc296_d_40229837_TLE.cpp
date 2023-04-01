526f2d4aa1c900a9fe5e9fdf033c732378c86afa
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-01 12:24:24 +0000

    [TLE] SubmissionID: 40229837 at 2023-04-01 12:24:24 (1680351864)

---
+++ abc296/abc296_d_40229837_TLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc296/submissions/40229837
+ * Submitted at: 2023-04-01 12:24:24
+ * Problem URL: https://atcoder.jp/contests/abc296/tasks/abc296_d
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+// code not found

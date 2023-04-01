1e50f7b2ff47580a086a0ec8d67e121e8712ab9b
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-01 04:28:39 +0000

    [RE] SubmissionID: 40197353 at 2023-04-01 04:28:39 (1680323319)

---
+++ abc235/abc235_d_40197353_RE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc235/submissions/40197353
+ * Submitted at: 2023-04-01 04:28:39
+ * Problem URL: https://atcoder.jp/contests/abc235/tasks/abc235_d
+ * Result: RE
+ * Execution Time: 108 ms
+ */
+
+// code not found

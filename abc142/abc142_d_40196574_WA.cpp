bbebe9b745345e9ae9ad397c22b0068b0cb2bd77
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-01 03:38:55 +0000

    [WA] SubmissionID: 40196574 at 2023-04-01 03:38:55 (1680320335)

---
+++ abc142/abc142_d_40196574_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc142/submissions/40196574
+ * Submitted at: 2023-04-01 03:38:55
+ * Problem URL: https://atcoder.jp/contests/abc142/tasks/abc142_d
+ * Result: WA
+ * Execution Time: 31 ms
+ */
+
+// code not found

8ac056489205b14541b70172440c39d0ea79fbd6
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-01 12:39:56 +0000

    [WA] SubmissionID: 40238198 at 2023-04-01 12:39:56 (1680352796)

---
+++ abc296/abc296_d_40238198_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc296/submissions/40238198
+ * Submitted at: 2023-04-01 12:39:56
+ * Problem URL: https://atcoder.jp/contests/abc296/tasks/abc296_d
+ * Result: WA
+ * Execution Time: 516 ms
+ */
+
+// code not found

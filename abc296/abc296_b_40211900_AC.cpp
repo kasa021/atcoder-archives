30b1db2b82c9acfcee4a19335edaec80fd8dd4f9
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-01 12:04:17 +0000

    [AC] SubmissionID: 40211900 at 2023-04-01 12:04:17 (1680350657)

---
+++ abc296/abc296_b_40211900_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc296/submissions/40211900
+ * Submitted at: 2023-04-01 12:04:17
+ * Problem URL: https://atcoder.jp/contests/abc296/tasks/abc296_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+// code not found

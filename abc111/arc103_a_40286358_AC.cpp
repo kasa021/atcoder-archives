1f6532aeb35f89a316064b6e9290ca841937038b
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-02 06:41:38 +0000

    [AC] SubmissionID: 40286358 at 2023-04-02 06:41:38 (1680417698)

---
+++ abc111/arc103_a_40286358_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc111/submissions/40286358
+ * Submitted at: 2023-04-02 06:41:38
+ * Problem URL: https://atcoder.jp/contests/abc111/tasks/arc103_a
+ * Result: AC
+ * Execution Time: 67 ms
+ */
+
+// code not found

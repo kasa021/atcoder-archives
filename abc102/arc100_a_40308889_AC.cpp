514252e9429d2a4689ca86b05b61b6b7e4f95c8d
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-03 07:56:05 +0000

    [AC] SubmissionID: 40308889 at 2023-04-03 07:56:05 (1680508565)

---
+++ abc102/arc100_a_40308889_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc102/submissions/40308889
+ * Submitted at: 2023-04-03 07:56:05
+ * Problem URL: https://atcoder.jp/contests/abc102/tasks/arc100_a
+ * Result: AC
+ * Execution Time: 75 ms
+ */
+
+// code not found

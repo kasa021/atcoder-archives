5b1cef8aaa13de2c45346378e2c6361d38d16c01
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-04 07:09:09 +0000

    [WA] SubmissionID: 40328881 at 2023-04-04 07:09:09 (1680592149)

---
+++ arc120/arc120_b_40328881_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc120/submissions/40328881
+ * Submitted at: 2023-04-04 07:09:09
+ * Problem URL: https://atcoder.jp/contests/arc120/tasks/arc120_b
+ * Result: WA
+ * Execution Time: 15 ms
+ */
+
+// code not found

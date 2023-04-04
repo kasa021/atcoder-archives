8a0152363adb3d43d42ee249177844735db7eafd
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-04 07:14:32 +0000

    [AC] SubmissionID: 40328970 at 2023-04-04 07:14:32 (1680592472)

---
+++ arc120/arc120_b_40328970_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc120/submissions/40328970
+ * Submitted at: 2023-04-04 07:14:32
+ * Problem URL: https://atcoder.jp/contests/arc120/tasks/arc120_b
+ * Result: AC
+ * Execution Time: 13 ms
+ */
+
+// code not found

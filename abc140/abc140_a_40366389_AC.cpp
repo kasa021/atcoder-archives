46c597ee2b4a54ed017e31ead841738da930f2ee
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-06 08:27:08 +0000

    [AC] SubmissionID: 40366389 at 2023-04-06 08:27:08 (1680769628)

---
+++ abc140/abc140_a_40366389_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc140/submissions/40366389
+ * Submitted at: 2023-04-06 08:27:08
+ * Problem URL: https://atcoder.jp/contests/abc140/tasks/abc140_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+// code not found

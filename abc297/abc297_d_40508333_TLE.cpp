0c41c2d0fb04a015ea23911f183d2ba039fb0996
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-10 06:12:10 +0000

    [TLE] SubmissionID: 40508333 at 2023-04-10 06:12:10 (1681107130)

---
+++ abc297/abc297_d_40508333_TLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc297/submissions/40508333
+ * Submitted at: 2023-04-10 06:12:10
+ * Problem URL: https://atcoder.jp/contests/abc297/tasks/abc297_d
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+// code not found

44e064e60bf7fdd7b415653e58e948aaa1043e3c
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-10 05:54:09 +0000

    [AC] SubmissionID: 40508033 at 2023-04-10 05:54:09 (1681106049)

---
+++ abc297/abc297_a_40508033_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc297/submissions/40508033
+ * Submitted at: 2023-04-10 05:54:09
+ * Problem URL: https://atcoder.jp/contests/abc297/tasks/abc297_a
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+// code not found

e84498c3f47c4ea29faba96de7854dd4357e3b9f
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-14 05:02:36 +0000

    [TLE] SubmissionID: 40600438 at 2023-04-14 05:02:36 (1681448556)

---
+++ abc119/abc119_c_40600438_TLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc119/submissions/40600438
+ * Submitted at: 2023-04-14 05:02:36
+ * Problem URL: https://atcoder.jp/contests/abc119/tasks/abc119_c
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+// code not found

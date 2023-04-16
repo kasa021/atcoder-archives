9a6e69918448375c053a7ed85731bdcc3fb63219
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-16 08:47:54 +0000

    [TLE] SubmissionID: 40698871 at 2023-04-16 08:47:54 (1681634874)

---
+++ abc097/arc097_a_40698871_TLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc097/submissions/40698871
+ * Submitted at: 2023-04-16 08:47:54
+ * Problem URL: https://atcoder.jp/contests/abc097/tasks/arc097_a
+ * Result: TLE
+ * Execution Time: 2354 ms
+ */
+
+// code not found

b66fdff91d8aa2dbdce25eaae4bf0eebf1c621a1
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-16 08:56:47 +0000

    [AC] SubmissionID: 40699116 at 2023-04-16 08:56:47 (1681635407)

---
+++ abc097/arc097_a_40699116_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc097/submissions/40699116
+ * Submitted at: 2023-04-16 08:56:47
+ * Problem URL: https://atcoder.jp/contests/abc097/tasks/arc097_a
+ * Result: AC
+ * Execution Time: 14 ms
+ */
+
+// code not found

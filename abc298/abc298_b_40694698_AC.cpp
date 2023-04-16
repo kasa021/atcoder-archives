89cb9d7ecf0989c2d5a8401bc040b0c271a73bef
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-16 06:13:16 +0000

    [AC] SubmissionID: 40694698 at 2023-04-16 06:13:16 (1681625596)

---
+++ abc298/abc298_b_40694698_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc298/submissions/40694698
+ * Submitted at: 2023-04-16 06:13:16
+ * Problem URL: https://atcoder.jp/contests/abc298/tasks/abc298_b
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+// code not found

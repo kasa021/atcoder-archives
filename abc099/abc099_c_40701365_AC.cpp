c46e6c82d8387902844f3a5e453dd3a505caf04f
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-16 10:36:14 +0000

    [AC] SubmissionID: 40701365 at 2023-04-16 10:36:14 (1681641374)

---
+++ abc099/abc099_c_40701365_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc099/submissions/40701365
+ * Submitted at: 2023-04-16 10:36:14
+ * Problem URL: https://atcoder.jp/contests/abc099/tasks/abc099_c
+ * Result: AC
+ * Execution Time: 17 ms
+ */
+
+// code not found

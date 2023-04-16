51e2a871e68cde2efe60ddd73a716c78d67f99d6
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-16 06:25:15 +0000

    [WA] SubmissionID: 40695023 at 2023-04-16 06:25:15 (1681626315)

---
+++ abc298/abc298_c_40695023_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc298/submissions/40695023
+ * Submitted at: 2023-04-16 06:25:15
+ * Problem URL: https://atcoder.jp/contests/abc298/tasks/abc298_c
+ * Result: WA
+ * Execution Time: 221 ms
+ */
+
+// code not found

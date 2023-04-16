7798000ae16de89de61903b0769c76a9cd7dcdf5
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-16 08:23:22 +0000

    [AC] SubmissionID: 40698179 at 2023-04-16 08:23:22 (1681633402)

---
+++ abc244/abc244_e_40698179_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc244/submissions/40698179
+ * Submitted at: 2023-04-16 08:23:22
+ * Problem URL: https://atcoder.jp/contests/abc244/tasks/abc244_e
+ * Result: AC
+ * Execution Time: 60 ms
+ */
+
+// code not found

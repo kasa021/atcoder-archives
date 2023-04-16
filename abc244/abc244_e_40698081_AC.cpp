a5721d02a29078db21d7acc65f25d2f6816e0373
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-16 08:19:52 +0000

    [AC] SubmissionID: 40698081 at 2023-04-16 08:19:52 (1681633192)

---
+++ abc244/abc244_e_40698081_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc244/submissions/40698081
+ * Submitted at: 2023-04-16 08:19:52
+ * Problem URL: https://atcoder.jp/contests/abc244/tasks/abc244_e
+ * Result: AC
+ * Execution Time: 353 ms
+ */
+
+// code not found

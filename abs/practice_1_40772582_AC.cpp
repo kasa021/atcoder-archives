2f249833f1c261e6e1d34a29d900e2c7137952f4
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-19 15:02:05 +0000

    [AC] SubmissionID: 40772582 at 2023-04-19 15:02:05 (1681916525)

---
+++ abs/practice_1_40772582_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abs/submissions/40772582
+ * Submitted at: 2023-04-19 15:02:05
+ * Problem URL: https://atcoder.jp/contests/abs/tasks/practice_1
+ * Result: AC
+ * Execution Time: 11 ms
+ */
+
+// code not found

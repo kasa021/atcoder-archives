6a39286efef42e221a94b61e35f731130217add2
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-20 13:08:48 +0000

    [AC] SubmissionID: 40790717 at 2023-04-20 13:08:48 (1681996128)

---
+++ abs/abc086_a_40790717_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abs/submissions/40790717
+ * Submitted at: 2023-04-20 13:08:48
+ * Problem URL: https://atcoder.jp/contests/abs/tasks/abc086_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+// code not found

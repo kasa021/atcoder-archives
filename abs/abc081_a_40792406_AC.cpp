74aaecd63675cfc2ca755cea29ce529c705a60f6
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-20 14:19:24 +0000

    [AC] SubmissionID: 40792406 at 2023-04-20 14:19:24 (1682000364)

---
+++ abs/abc081_a_40792406_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abs/submissions/40792406
+ * Submitted at: 2023-04-20 14:19:24
+ * Problem URL: https://atcoder.jp/contests/abs/tasks/abc081_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+// code not found

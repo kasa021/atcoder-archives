f427010f1bf9f43e58616f066fee1cef8fd3d10b
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-22 12:28:38 +0000

    [WA] SubmissionID: 40851370 at 2023-04-22 12:28:38 (1682166518)

---
+++ abc299/abc299_c_40851370_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc299/submissions/40851370
+ * Submitted at: 2023-04-22 12:28:38
+ * Problem URL: https://atcoder.jp/contests/abc299/tasks/abc299_c
+ * Result: WA
+ * Execution Time: 13 ms
+ */
+
+// code not found

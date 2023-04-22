f8776d46fb10ee1e7b82b2fa74df7458e0af551f
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-22 12:04:39 +0000

    [AC] SubmissionID: 40832427 at 2023-04-22 12:04:39 (1682165079)

---
+++ abc299/abc299_a_40832427_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc299/submissions/40832427
+ * Submitted at: 2023-04-22 12:04:39
+ * Problem URL: https://atcoder.jp/contests/abc299/tasks/abc299_a
+ * Result: AC
+ * Execution Time: 15 ms
+ */
+
+// code not found

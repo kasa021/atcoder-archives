2fd1fc5953bb9036afb58936392c31f0c8f4bde2
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-22 12:46:04 +0000

    [AC] SubmissionID: 40860977 at 2023-04-22 12:46:04 (1682167564)

---
+++ abc299/abc299_d_40860977_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc299/submissions/40860977
+ * Submitted at: 2023-04-22 12:46:04
+ * Problem URL: https://atcoder.jp/contests/abc299/tasks/abc299_d
+ * Result: AC
+ * Execution Time: 16 ms
+ */
+
+// code not found

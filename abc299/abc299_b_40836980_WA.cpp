b09e80698281af95b41f1e36b56e51058c9578fc
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-22 12:09:47 +0000

    [WA] SubmissionID: 40836980 at 2023-04-22 12:09:47 (1682165387)

---
+++ abc299/abc299_b_40836980_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc299/submissions/40836980
+ * Submitted at: 2023-04-22 12:09:47
+ * Problem URL: https://atcoder.jp/contests/abc299/tasks/abc299_b
+ * Result: WA
+ * Execution Time: 117 ms
+ */
+
+// code not found

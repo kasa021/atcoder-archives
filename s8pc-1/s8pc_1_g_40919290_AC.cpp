20093fa636b23bf0e0a66e0cd231728be53c6528
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-24 08:47:35 +0000

    [AC] SubmissionID: 40919290 at 2023-04-24 08:47:35 (1682326055)

---
+++ s8pc-1/s8pc_1_g_40919290_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/s8pc-1/submissions/40919290
+ * Submitted at: 2023-04-24 08:47:35
+ * Problem URL: https://atcoder.jp/contests/s8pc-1/tasks/s8pc_1_g
+ * Result: AC
+ * Execution Time: 69 ms
+ */
+
+// code not found

6a99cb77dfd29344e36c337c4e508f178633fd6f
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-25 13:21:39 +0000

    [AC] SubmissionID: 40944549 at 2023-04-25 13:21:39 (1682428899)

---
+++ joi2014yo/joi2014yo_d_40944549_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/joi2014yo/submissions/40944549
+ * Submitted at: 2023-04-25 13:21:39
+ * Problem URL: https://atcoder.jp/contests/joi2014yo/tasks/joi2014yo_d
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+// code not found

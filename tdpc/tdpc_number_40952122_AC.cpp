9b590a12b9d088a2f448411276cebd408b75d1c6
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-26 02:39:06 +0000

    [AC] SubmissionID: 40952122 at 2023-04-26 02:39:06 (1682476746)

---
+++ tdpc/tdpc_number_40952122_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/tdpc/submissions/40952122
+ * Submitted at: 2023-04-26 02:39:06
+ * Problem URL: https://atcoder.jp/contests/tdpc/tasks/tdpc_number
+ * Result: AC
+ * Execution Time: 73 ms
+ */
+
+// code not found

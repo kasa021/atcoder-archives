8dd3ea4d2112839fc37de995a8264bd2a2376b77
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-04-29 15:56:34 +0000

    [WA] SubmissionID: 41057436 at 2023-04-29 15:56:34 (1682783794)

---
+++ arc148/arc148_a_41057436_WA.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/arc148/submissions/41057436
+ * Submitted at: 2023-04-29 15:56:34
+ * Problem URL: https://atcoder.jp/contests/arc148/tasks/arc148_a
+ * Result: WA
+ * Execution Time: 72 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+
+int main()
+{
+    ll n,greatestCommonDiviser;
+    cin>>n;
+    vector<ll> a(n);
+    rep(i,n) cin>>a[i];
+    greatestCommonDiviser=abs(a[1]-a[0]);
+    rep(i,n-1) {
+        greatestCommonDiviser = __gcd(greatestCommonDiviser, abs(a[i+1]-a[i]));
+        // cout<< greatestCommonDiviser << endl;
+    }
+    // cout<< greatestCommonDiviser << endl;
+    if(greatestCommonDiviser>1)cout<< 1 << endl;
+    else cout<< 2 << endl;
+}

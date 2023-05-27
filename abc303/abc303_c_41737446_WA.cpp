bacea2d1f814ce80423e64e1539000c8b8dfdd41
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-05-27 12:13:55 +0000

    [WA] SubmissionID: 41737446 at 2023-05-27 12:13:55 (1685189635)

---
+++ abc303/abc303_c_41737446_WA.cpp
@@ -0,0 +1,53 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc303/submissions/41737446
+ * Submitted at: 2023-05-27 12:13:55
+ * Problem URL: https://atcoder.jp/contests/abc303/tasks/abc303_c
+ * Result: WA
+ * Execution Time: 196 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+    int n, m, h, k;
+    cin >> n >> m >> h >> k;
+    string s;
+    cin >> s;
+    set<P> st;
+    rep(i,m){
+        int a,b;
+        cin>>a>>b;
+        st.insert(P(a,b));
+    }
+    int x=0,y=0;
+    rep(i,n){
+        if(s[i]=='L')x--;
+        if(s[i]=='R')x++;
+        if(s[i]=='U')y++;
+        if(s[i]=='D')y--;
+        h--;
+        if(h<0){
+            cout<<"No"<<endl;
+            return 0;
+        }else if(h<k && st.count(P(x,y))){
+            h=k;
+        }
+    }
+    cout<<"Yes"<<endl;
+}

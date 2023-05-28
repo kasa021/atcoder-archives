b668994798fb6f99deb449c0db19f15f88f1b8e8
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-05-28 19:24:57 +0000

    [AC] SubmissionID: 41820158 at 2023-05-28 19:24:57 (1685301897)

---
+++ abc106/abc106_b_41820158_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc106/submissions/41820158
+ * Submitted at: 2023-05-28 19:24:57
+ * Problem URL: https://atcoder.jp/contests/abc106/tasks/abc106_b
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+
+int prime_factorize(int n){ // 約数分解
+    int res = 0;
+    for (int i = 1; i <= n; i++){
+        if (n % i == 0) res++;  // 約数の個数をカウント
+    }
+    return res;
+}
+
+int main() {
+    int n,ans=0;
+    cin >> n;
+    for(int i=1;i<=n;i+=2){  // 奇数のみ
+        if(prime_factorize(i)==8) ans++;
+    }
+    cout << ans << endl;
+}

d89e09ebf4299163e1972425100e8f70ac6961ed
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-06-22 14:54:34 +0000

    [WA] SubmissionID: 42832318 at 2023-06-22 14:54:34 (1687445674)

---
+++ agc029/agc029_a_42832318_WA.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/agc029/submissions/42832318
+ * Submitted at: 2023-06-22 14:54:34
+ * Problem URL: https://atcoder.jp/contests/agc029/tasks/agc029_a
+ * Result: WA
+ * Execution Time: 11 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+
+int main() {
+  string s;
+  cin >> s;
+  int ans = 0, cnt_b = 0;
+  rep(i,s.size()){
+    if(s[i] == 'B') cnt_b++;
+    else ans += cnt_b;
+  }
+  cout << ans << endl;
+}

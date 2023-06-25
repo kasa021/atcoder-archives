badab4f1032ed38963f8463cda54d631b6a24aba
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-06-25 15:06:44 +0000

    [AC] SubmissionID: 42965554 at 2023-06-25 15:06:44 (1687705604)

---
+++ typical90/typical90_j_42965554_AC.cpp
@@ -0,0 +1,58 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/42965554
+ * Submitted at: 2023-06-25 15:06:44
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_j
+ * Result: AC
+ * Execution Time: 220 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> sum1(n+1, 0);
+  vector<int> sum2(n+1, 0);
+  rep(i,n){
+    int c, p;
+    cin >> c >> p;
+    // クラスごとの累積和を求める
+    if(c == 1){
+      sum1[i+1] = sum1[i] + p;  
+      sum2[i+1] = sum2[i];
+    }
+    else{
+      sum1[i+1] = sum1[i];
+      sum2[i+1] = sum2[i] + p;
+    }
+  }
+
+  int q;
+  cin >> q;
+  rep(i,q){
+    int l, r;
+    cin >> l >> r;
+    l--; r--;  // 0-indexedにする
+    // 累積和の差分を求める
+    int ans1 = sum1[r+1] - sum1[l];
+    int ans2 = sum2[r+1] - sum2[l];
+    cout << ans1 << " " << ans2 << endl;
+  }
+}

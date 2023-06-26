ea729dc86750c964a355df38e2328ef6de1cd7d3
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-06-26 06:06:03 +0000

    [WA] SubmissionID: 42974844 at 2023-06-26 06:06:03 (1687759563)

---
+++ typical90/typical90_ag_42974844_WA.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/42974844
+ * Submitted at: 2023-06-26 06:06:03
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_ag
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+
+int main() {
+  int h, w;
+  cin>>h>>w;
+  int ans = 0;
+  for (int i = 0; i < h; i += 2) {
+    for (int j = 0; j < w;j+=2){
+      ans++;
+    }
+  }
+  cout << ans << endl;
+}

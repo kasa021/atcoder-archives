a1d5f432d2b64a049408672fcdbbc9cf9ea70a65
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-06-30 11:51:58 +0000

    [AC] SubmissionID: 43066927 at 2023-06-30 11:51:58 (1688125918)

---
+++ abc109/abc109_b_43066927_AC.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/abc109/submissions/43066927
+ * Submitted at: 2023-06-30 11:51:58
+ * Problem URL: https://atcoder.jp/contests/abc109/tasks/abc109_b
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (int i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using graph = vector<vector<int>>;
+
+int main() {
+  int n;
+  cin >> n;
+  set<string> st;
+  string prev = "";
+  rep(i,n){
+    if(prev.size()==0){
+      string s;
+      cin >> s;
+      prev = s;
+      st.insert(s);
+    }else {
+      string s;
+      cin >> s;
+      if(st.count(s)==0 && prev[prev.size()-1]==s[0]){
+        st.insert(s);
+        prev = s;
+      }else {
+        cout << "No" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "Yes" << endl;
+}

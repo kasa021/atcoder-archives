4309e4d203329886013a29028b23400b9e2bf437
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-07-02 18:04:32 +0000

    [AC] SubmissionID: 43206642 at 2023-07-02 18:04:32 (1688321072)

---
+++ panasonic2020/panasonic2020_c_43206642_AC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/panasonic2020/submissions/43206642
+ * Submitted at: 2023-07-02 18:04:32
+ * Problem URL: https://atcoder.jp/contests/panasonic2020/tasks/panasonic2020_c
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+using graph = vector<vector<ll>>;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (ll i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  if(4*a*b < (c-a-b)*(c-a-b) && c-a-b > 0) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}

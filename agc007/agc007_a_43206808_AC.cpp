65ae8157afdaddda74c04b15ed0edb7b580899ba
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-07-02 18:23:02 +0000

    [AC] SubmissionID: 43206808 at 2023-07-02 18:23:02 (1688322182)

---
+++ agc007/agc007_a_43206808_AC.cpp
@@ -0,0 +1,64 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/agc007/submissions/43206808
+ * Submitted at: 2023-07-02 18:23:02
+ * Problem URL: https://atcoder.jp/contests/agc007/tasks/agc007_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+using graph = vector<vector<ll>>;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (ll i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+vector<int> mx = {1,0};
+vector<int> my = {0,1};
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vector<string> s(h);
+  rep(i, h) cin >> s[i];
+
+  vector<vector<bool>> visited(h, vector<bool>(w, false));
+  vector<vector<int>> dist(h, vector<int>(w, -1));
+  queue<P> que;
+  que.push({0, 0});
+  visited[0][0] = true;
+  dist[0][0] = 0;
+  while(!que.empty()){
+    auto [x,y] = que.front();
+    que.pop();
+    rep(i,2){
+      int nx = x + mx[i];
+      int ny = y + my[i];
+      if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+      if(s[nx][ny] == '.') continue;
+      if(visited[nx][ny]) continue;
+      que.push({nx,ny});
+      visited[nx][ny] = true;
+      dist[nx][ny] = dist[x][y] + 1;
+    }
+  }
+  int cnt = 0;
+  rep(i,h){
+    rep(j,w){
+      if(s[i][j] == '#') cnt++;
+    }
+  }
+  if(dist[h-1][w-1] == cnt-1) cout << "Possible" << endl;
+  else cout << "Impossible" << endl;
+}

d37718b7166521eea2bd8992f11210f93a3372d3
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-07-02 10:18:29 +0000

    [WA] SubmissionID: 43182049 at 2023-07-02 10:18:29 (1688293109)

---
+++ agc008/agc008_a_43182049_WA.cpp
@@ -0,0 +1,54 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/agc008/submissions/43182049
+ * Submitted at: 2023-07-02 10:18:29
+ * Problem URL: https://atcoder.jp/contests/agc008/tasks/agc008_a
+ * Result: WA
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (ll i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+using graph = vector<vector<ll>>;
+
+int main() {
+  ll x, y;
+  cin >> x >> y;
+  ll ans = 0;
+  if (x >= 0 && y >= 0) {
+    if (x <= y) ans = y - x;
+    else
+      ans = 2 + x - y;
+  }
+  else if (x >= 0 && y < 0) {
+    if (abs(x) == abs(y)) ans = 1;
+    else if (abs(x) > abs(y))
+      ans = 1 + x + y;
+    else
+      ans = 1 - y - x;
+  }
+  else if (x < 0 && y >= 0) {
+    if (abs(x) == abs(y)) ans = 1;
+    else if (abs(x) > abs(y))
+      ans = y - x;
+    else
+      ans = 1 + x + y;
+  }else {
+    if (x < y) ans = abs(y - x);
+    else ans = 2+ abs(x - y);
+  }
+  cout << ans << endl;
+}

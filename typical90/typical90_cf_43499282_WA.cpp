2bbd69133ee4f2a3817c28fd51ab1d61fd5d761c
kasa021 <109140298+kasa021@users.noreply.github.com>
2023-07-12 04:17:14 +0000

    [WA] SubmissionID: 43499282 at 2023-07-12 04:17:14 (1689135434)

---
+++ typical90/typical90_cf_43499282_WA.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: kAsA02
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/43499282
+ * Submitted at: 2023-07-12 04:17:14
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_cf
+ * Result: WA
+ * Execution Time: 49 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+typedef vector<vector<ll>> graph;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep1(i, s, n) for (ll i = s; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+  int n;
+  string a;
+  cin >> n >> a;
+  vector<pair<char, ll>> rle(0);
+  rle.push_back({ a[0], 0 });
+  rep(i,a.size()){
+    if(rle.back().first == a[i]) rle.back().second++;
+    else rle.push_back({ a[i], 1 });
+  }
+
+  ll ans = n*(n+1)/2;
+  for(auto p : rle){
+    ans -= p.second*(p.second+1)/2;
+  }
+  cout << ans << endl;
+}
